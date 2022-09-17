56ce1a50b4bf838d0220c99aa1d0cc2a7935fbb4
brachkow <33203187+brachkow@users.noreply.github.com>
2022-09-17 10:56:20 +0200

    Avatar component added

---
+++ src/components/Avatar.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+  defineProps({
+    name: { type: String, default: '' },
+    surname: { type: String, default: '' },
+  });
+</script>
+
+<template>
+  <div
+    class="flex h-32 w-32 items-center justify-center rounded-full bg-black uppercase text-white">
+    {{ name[0] }}{{ surname[0] }}
+  </div>
+</template>
