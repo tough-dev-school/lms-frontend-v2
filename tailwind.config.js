2e0c8bec3cdb3d1c5fac44e553097374a43b7736
brachkow <33203187+brachkow@users.noreply.github.com>
2022-09-20 23:46:46 +0200

    basic tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,50 @@
+const spacing = (base, limit, unit = 'px') => {
+  const output = {};
+  let n = 1;
+  let value;
+
+  do {
+    value = base * n;
+    output[value] = `${value}${unit}`;
+    n++;
+  } while (value <= limit);
+
+  return output;
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,vue,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      display: 'serif',
+      body: 'sans-serif',
+    },
+    colors: {
+      black: { DEFAULT: '#000000' },
+      white: { DEFAULT: '#FFFFFF' },
+      yellow: { DEFAULT: '#ffa800', hover: '#ffa800' },
+      blue: { DEFAULT: '#3f5dff', hover: '#0024de' },
+      gray: { DEFAULT: '#9a9a9a' },
+      red: { DEFAULT: 'red' },
+    },
+    fontSize: {
+      sub: '14px',
+      base: '16px',
+      h1: '36px',
+      h2: '24px',
+      h3: '16px',
+    },
+    borderRadius: {
+      DEFAULT: '16px',
+      row: '28px',
+      full: '50%',
+    },
+    spacing: {
+      0: 0,
+      4: 4,
+      ...spacing(8, 512),
+    },
     extend: {},
   },
   plugins: [],
