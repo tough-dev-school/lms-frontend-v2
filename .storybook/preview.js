3b559d2be72600fba47190e33fb426e64c09699b
brachkow <33203187+brachkow@users.noreply.github.com>
2022-09-21 21:25:50 +0200

    styles added to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,13 @@
+import '../src/fonts.css';
+import '../src/main.scss';
+import '../src/tailwind.css';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
