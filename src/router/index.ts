7204fa9ed3e0b664a7b0af5d845f8ca768afd404
brachkow <33203187+brachkow@users.noreply.github.com>
2022-09-27 00:09:45 +0200

    auth route guard added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ProfileView from '@/views/ProfileView.vue';
 import LoginView from '@/views/LoginView.vue';
+import useAuth from '@/stores/auth';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,19 @@ const router = createRouter({
       component: LoginView,
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const auth = useAuth();
+
+  const isAuthorized = !!auth.token;
+  const isWhitelisted = ['login'].includes(to.name);
+
+  if (isAuthorized || isWhitelisted) {
+    next();
+  } else {
+    next('/login');
+  }
 });
 
 export default router;
