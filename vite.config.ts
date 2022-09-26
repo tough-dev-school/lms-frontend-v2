a87b8a62a679d2f07e94227735de2dc0edff43ef
brachkow <33203187+brachkow@users.noreply.github.com>
2022-09-26 23:41:31 +0200

    proxy added

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    proxy: {
+      '/api': 'http://localhost:8000',
+    },
   },
   plugins: [vue(), vueJsx()],
   resolve: {
