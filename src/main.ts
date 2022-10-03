675346cb38f54ac2df0bea23de67fdef71f7c405
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-10-03 19:01:44 +0200

    auth store is persisted (#20)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
 import App from './App.vue';
 import router from './router';
@@ -10,7 +11,10 @@ import './main.scss';
 
 const app = createApp(App);
 
-app.use(createPinia());
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
+
 app.use(router);
 
 app.mount('#app');
