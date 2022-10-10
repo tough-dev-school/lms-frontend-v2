63dac6a58303113ce3d10631a7d28750eb62a9ee
brachkow <33203187+brachkow@users.noreply.github.com>
2022-10-10 18:25:29 +0200

    basic visual test

---
+++ src/visual-regression.test.js
@@ -0,0 +1,29 @@
+import { expect, describe, test, beforeEach } from 'vitest';
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
+import puppeteer from 'puppeteer';
+
+expect.extend({ toMatchImageSnapshot });
+
+describe('visual regression test', () => {
+  let browser;
+  let page;
+
+  beforeEach(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    page.setViewport({ width: 1440, height: 900 });
+  });
+
+  const goto = (route) => {
+    return page.goto(`http://localhost:6006${route}`, {
+      waitUntil: 'networkidle0',
+    });
+  };
+
+  test('LoginView', async () => {
+    await goto(`/iframe.html?args=&id=pages-loginview--default&viewMode=story`);
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+});
