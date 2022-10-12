9b2e6f2fb256cb6d6a51c551dcf425b8fe9d63a8
brachkow <33203187+brachkow@users.noreply.github.com>
2022-10-12 17:04:58 +0200

    test all target resolutions

--- src/visual-regression.test.js
+++ src/visual-regression.test.js
@@ -13,14 +13,54 @@ const matchConfig = {
   failureThresholdType: 'percent',
 };
 
-describe('visual regression test', () => {
+const DESKTOP_VIEWPORT = [1440, 900];
+const TABLET_VIEWPORT = [768, 1024];
+const MOBILE_VIEWPORT = [320, 560];
+
+class VisualTest {
+  constructor(name, path, action = () => {}) {
+    this.name = name;
+    this.path = path;
+    this.action = action;
+  }
+}
+
+describe('visual regression test for', () => {
   let browser;
   let page;
+
+  let tests = [];
+
+  [
+    new VisualTest(
+      'LoginView — email login',
+      `/iframe.html?args=&id=pages-app--login&viewMode=story#/login`,
+    ),
+    new VisualTest(
+      'LoginView — password login',
+      `/iframe.html?args=&id=pages-app--login&viewMode=story#/login`,
+      async () => {
+        await page.click('[data-testid="to-password-mode"]');
+      },
+    ),
+    new VisualTest(
+      'ProfileView',
+      `/iframe.html?args=&id=pages-app--profile&viewMode=story#/profile`,
+    ),
+  ].forEach((test) => {
+    [DESKTOP_VIEWPORT, TABLET_VIEWPORT, MOBILE_VIEWPORT].forEach((viewport) => {
+      tests.push([
+        `${test.name} — ${viewport[0]}×${viewport[1]}`,
+        test.path,
+        test.action,
+        ...viewport,
+      ]);
+    });
+  });
 
   beforeEach(async () => {
     browser = await puppeteer.launch();
     page = await browser.newPage();
-    page.setViewport({ width: 1440, height: 900 });
   });
 
   const goto = (route) => {
@@ -29,28 +69,11 @@ describe('visual regression test', () => {
     });
   };
 
-  test('LoginView — email login', async () => {
-    await goto(`/iframe.html?args=&id=pages-app--login&viewMode=story#/login`);
+  test.each(tests)('%s', async (name, route, action, width, height) => {
+    await page.setViewport({ width, height });
+    await goto(route);
 
-    const image = await page.screenshot();
-
-    expect(image).toMatchImageSnapshot(matchConfig);
-  });
-
-  test('LoginView — password login', async () => {
-    await goto(`/iframe.html?args=&id=pages-app--login&viewMode=story#/login`);
-
-    await page.click('[data-testid="to-password-mode"]');
-    const image = await page.screenshot();
-
-    expect(image).toMatchImageSnapshot(matchConfig);
-  });
-
-  test('ProfileView', async () => {
-    await goto(
-      `/iframe.html?args=&id=pages-app--profile&viewMode=story#/profile`,
-    );
-
+    await action();
     const image = await page.screenshot();
 
     expect(image).toMatchImageSnapshot(matchConfig);
