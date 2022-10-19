dc0d833177a089e49368c16b91b444e4e251ceaf
brachkow <33203187+brachkow@users.noreply.github.com>
2022-10-19 19:01:06 +0200

    dayjs locale changed

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,8 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ru';
+
 dayjs.extend(relativeTime);
+dayjs.locale('ru');
 
 export const relativeDate = (date: dayjs.ConfigType) => dayjs().to(date);
