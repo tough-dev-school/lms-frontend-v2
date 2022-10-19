6139162647b5174d9787e3246c29d1b4a73ea711
brachkow <33203187+brachkow@users.noreply.github.com>
2022-10-19 17:46:39 +0200

    homework-details component added

---
+++ src/components/HomeworkDetails.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+  import { defineProps } from 'vue';
+
+  defineProps({ task: { type: String } });
+</script>
+
+<template>
+  <details>
+    <summary>Показать задание</summary>
+    <div v-text="task" />
+  </details>
+</template>
