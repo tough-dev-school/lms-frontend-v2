9b618d60bc40bc005011a41c7edc127986dcabf0
brachkow <33203187+brachkow@users.noreply.github.com>
2022-10-25 22:41:27 +0200

    filter refactoring

---
+++ src/utils/filterDictionary.ts
@@ -0,0 +1,15 @@
+export interface Dictionary<T> {
+  [k: string]: T;
+}
+
+export default function filterDictionary<T>(
+  inputDictionary: Dictionary<T>,
+  filterFunction: (value: T, key: string) => boolean,
+): Dictionary<T> {
+  const outDict: Dictionary<T> = {};
+  for (const k of Object.keys(inputDictionary)) {
+    const thisValue = inputDictionary[k];
+    if (filterFunction(thisValue, k)) outDict[k] = thisValue;
+  }
+  return outDict;
+}
