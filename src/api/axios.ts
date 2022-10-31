b4dd331d91cbf892cd6275751810da2469e8b880
brachkow <33203187+brachkow@users.noreply.github.com>
2022-10-31 20:18:44 +0100

    fix materials backend

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,27 +3,33 @@ import axios from 'axios';
 import handleError from '@/utils/handleError';
 import applyCaseMiddleware from 'axios-case-converter';
 
-const instance = applyCaseMiddleware(axios.create());
+export const createCustomAxiosInstance = ({ useCaseMiddleware = true }) => {
+  const instance = useCaseMiddleware
+    ? applyCaseMiddleware(axios.create())
+    : axios.create();
 
-instance.interceptors.request.use((request) => {
-  const auth = useAuth();
-  request.headers = {
-    ...request.headers,
-  };
+  instance.interceptors.request.use((request) => {
+    const auth = useAuth();
+    request.headers = {
+      ...request.headers,
+    };
 
-  if (auth.token) request.headers.Authorization = `Bearer ${auth.token}`;
+    if (auth.token) request.headers.Authorization = `Bearer ${auth.token}`;
 
-  return request;
-});
+    return request;
+  });
 
-instance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    handleError(error);
-    return Promise.reject(error);
-  },
-);
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      handleError(error);
+      return Promise.reject(error);
+    },
+  );
 
-export default instance;
+  return instance;
+};
+
+export default createCustomAxiosInstance({ useCaseMiddleware: true });
