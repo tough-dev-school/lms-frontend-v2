e9c766880399d90c014512af8b6e0fe7d31e4ce3
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-11-09 02:39:13 +0100

    homework store refactored

--- src/stores/homework.ts
+++ src/stores/homework.ts
@@ -1,25 +1,24 @@
 import { defineStore } from 'pinia';
 import {
   getQuestion,
-  getAnswer,
   postAnswer,
   deleteAnswer,
   updateAnswer,
   getAnswers,
+  getAnswer,
 } from '@/api/homework';
 import type { Answer, Question } from '@/types/homework';
-import useUser from '@/stores/user';
 
 interface State {
   question: Question | undefined;
-  answer: Answer | undefined;
+  answers: Answer[];
 }
 
 const useHomework = defineStore('homework', {
   state: (): State => {
     return {
       question: undefined,
-      answer: undefined,
+      answers: [],
     };
   },
   actions: {
@@ -28,44 +27,41 @@ const useHomework = defineStore('homework', {
         this.question = await getQuestion(questionId);
       } catch (error: any) {}
     },
-    async getAnswer(answerId: string) {
+    async getAnswers({
+      questionId,
+      authorId,
+    }: {
+      questionId: string;
+      authorId: string;
+    }) {
       try {
-        this.answer = await getAnswer(answerId);
+        this.answers = (
+          await getAnswers({
+            questionId,
+            authorId,
+          })
+        ).results;
       } catch (error: any) {}
     },
-    async findAnswer(questionId: string, authorId: string) {
-      const answers = await getAnswers({
-        questionId,
-        authorId,
-      });
-
-      const lastAnswer = answers.results.at(-1);
-
-      let answerId = lastAnswer ? lastAnswer.slug : lastAnswer;
-
-      if (answerId) {
-        await this.getAnswer(answerId);
-      }
+    async getAnswerById(answerId: string) {
+      try {
+        const answer = await getAnswer(answerId);
+        this.answers = [answer];
+      } catch (error: any) {}
     },
     async postAnswer(text: string, questionId: string) {
       try {
         await postAnswer(text, questionId, null);
-
-        const user = useUser();
-        const authorId = user.uuid;
-        await this.findAnswer(questionId, authorId);
       } catch (error: any) {}
     },
     async deleteAnswer(answerId: string) {
       try {
         await deleteAnswer(answerId);
-        this.answer = undefined;
       } catch (error: any) {}
     },
     async updateAnswer(answerId: string, text: string) {
       try {
         await updateAnswer(answerId, text);
-        await this.getAnswer(answerId);
       } catch (error: any) {}
     },
   },
