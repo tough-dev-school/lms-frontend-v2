2d6a3458432ee59b3fdd886f45a13fbd83badda3
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-11-10 03:37:59 +0100

    reply moved to separate component

---
+++ src/components/VReply.vue
@@ -0,0 +1,73 @@
+<script lang="ts" setup>
+  import VHtmlContent from '@/components/VHtmlContent.vue';
+  import VTextEditor from '@/components/VTextEditor.vue';
+  import VButton from '@/components/VButton.vue';
+  import useHomework from '@/stores/homework';
+  import { ref } from 'vue';
+  import VAnswerActions from '@/components/VAnswerActions.vue';
+  import htmlToMarkdown from '@/utils/htmlToMarkdown';
+
+  const homework = useHomework();
+
+  const emit = defineEmits('update');
+
+  const props = defineProps({
+    reply: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    questionId: { type: String, required: true },
+  });
+
+  const editMode = ref(false);
+
+  const text = ref('');
+
+  const handleEditorUpdate = (value: string) => {
+    text.value = value;
+  };
+
+  const saveAnswer = async () => {
+    console.log(text.value);
+    if (props.reply.slug) {
+      await homework.updateAnswer(props.reply.slug, text.value);
+    } else if (props.questionId) {
+      await homework.postAnswer(text.value, props.questionId);
+    }
+    emit('update');
+    editMode.value = false;
+  };
+
+  const handleDelete = async () => {
+    if (!props.reply.text) return;
+    if (confirm('Удалить ответ?')) {
+      await homework.deleteAnswer(props.reply.slug);
+      emit('update');
+    }
+  };
+
+  const handleEdit = async () => {
+    if (!props.reply.text) return;
+    handleEditorUpdate(htmlToMarkdown(props.reply.text));
+    editMode.value = true;
+  };
+</script>
+
+<template>
+  <div v-if="reply.slug && editMode === false">
+    <VHtmlContent :content="reply.text" />
+    <VAnswerActions
+      @delete="handleDelete"
+      @edit="handleEdit"
+      :date-added="reply.created" />
+  </div>
+  <div v-else>
+    <VTextEditor
+      @update="handleEditorUpdate"
+      :value="reply.text"
+      class="mb-16 rounded border border-gray" />
+    <VButton @click="saveAnswer">Отправить</VButton>
+  </div>
+</template>
--- src/views/VHomeworkQuestionView.vue
+++ src/views/VHomeworkQuestionView.vue
@@ -1,7 +1,5 @@
 <script lang="ts" setup>
   import VHtmlContent from '@/components/VHtmlContent.vue';
-  import VTextEditor from '@/components/VTextEditor.vue';
-  import VButton from '@/components/VButton.vue';
   import useHomework from '@/stores/homework';
   import { useRoute } from 'vue-router';
   import { computed, onMounted, ref } from 'vue';
@@ -9,9 +7,8 @@
   import { storeToRefs } from 'pinia';
   import VHeading from '@/components/VHeading.vue';
   import VPreloader from '@/components/VPreloader.vue';
+  import VReply from '@/components/VReply.vue';
   import useUser from '@/stores/user';
-  import VAnswerActions from '@/components/VAnswerActions.vue';
-  import htmlToMarkdown from '@/utils/htmlToMarkdown';
 
   const route = useRoute();
   const homework = useHomework();
@@ -23,38 +20,6 @@
   const answer = computed(() => {
     return answers.value.at(-1);
   });
-
-  const editMode = ref(false);
-
-  const text = ref('');
-
-  const handleEditorUpdate = (value: string) => {
-    text.value = value;
-  };
-
-  const saveAnswer = async () => {
-    if (answer.value) {
-      await homework.updateAnswer(answer.value.slug, text.value);
-    } else if (questionId.value) {
-      await homework.postAnswer(text.value, questionId.value);
-    }
-    await getData();
-    editMode.value = false;
-  };
-
-  const handleDelete = async () => {
-    if (!answer.value) return;
-    if (confirm('Удалить ответ?')) {
-      await homework.deleteAnswer(answer.value.slug);
-      await getData();
-    }
-  };
-
-  const handleEdit = async () => {
-    if (!answer.value) return;
-    handleEditorUpdate(htmlToMarkdown(answer.value.text));
-    editMode.value = true;
-  };
 
   const getData = async () => {
     questionId.value = String(route.params.questionId);
@@ -78,20 +43,12 @@
     </section>
     <section>
       <VHeading level="2" class="mb-24">Ответ</VHeading>
-      <div v-if="answer && !editMode" data-testid="answer">
-        <VHtmlContent :content="answer.text" />
-        <VAnswerActions
-          @delete="handleDelete"
-          @edit="handleEdit"
-          :date-added="answer.created" />
-      </div>
-      <div v-else data-testid="editor">
-        <VTextEditor
-          @update="handleEditorUpdate"
-          :value="text"
-          class="mb-16 rounded border border-gray" />
-        <VButton @click="saveAnswer" :disabled="!text">Отправить</VButton>
-      </div>
+      <VReply
+        :reply="answer"
+        v-if="answer"
+        @update="getData"
+        :questionId="questionId" />
+      <VReply v-else @update="getData" :questionId="questionId" />
     </section>
   </div>
   <VPreloader v-else />
