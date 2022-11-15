4091cb1f909929469322a8df2eac7c09ca83a861
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-11-15 14:59:54 +0100

    fix array being converted to object

--- src/utils/convertKeysToCamelCase.ts
+++ src/utils/convertKeysToCamelCase.ts
@@ -1,8 +1,10 @@
-import { isObject, mapKeys, camelCase } from 'lodash';
+import { isPlainObject, mapKeys, camelCase } from 'lodash';
 
 const convertKeysToCamelCase = (obj: Object) => {
   return JSON.parse(JSON.stringify(obj), (key, value) =>
-    isObject(value) ? mapKeys(value, (_value, _key) => camelCase(_key)) : value,
+    isPlainObject(value)
+      ? mapKeys(value, (_value, _key) => camelCase(_key))
+      : value,
   );
 };
 
