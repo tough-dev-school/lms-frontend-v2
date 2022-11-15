d5fde9df5ea00335eef51f259ede38c702e09de7
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-11-15 18:01:57 +0100

    VFloat added

---
+++ src/components/VFloat.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+  import { DotsVerticalIcon } from 'vue-tabler-icons';
+</script>
+
+<template>
+  <VDropdown placement="bottom-end">
+    <button class="text-gray transition-colors hover:text-black">
+      <DotsVerticalIcon />
+    </button>
+    <template #popper>
+      <slot />
+    </template>
+  </VDropdown>
+</template>
+
+<style>
+  .v-popper--theme-dropdown .v-popper__inner.v-popper__inner {
+    @apply border-none shadow;
+  }
+
+  .v-popper__arrow-container {
+    @apply hidden;
+  }
+</style>
