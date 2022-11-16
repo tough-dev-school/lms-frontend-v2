ae31abee70829beef79fed477361c58f2eaa3352
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-11-16 13:36:46 +0100

    AnswerActions refactoring

--- src/components/VAnswerActions.vue
+++ src/components/VAnswerActions.vue
@@ -13,47 +13,46 @@
 
   const emit = defineEmits(['delete', 'edit']);
 
-  export interface Action {
-    eventName: string;
-    label: string;
-  }
   const checkIsAvailable = (timeout: number) => {
     return +dayjs() < +dayjs(props.created).add(timeout, 'minutes');
   };
-
-  const actions: Action[] = [
-    { eventName: 'delete', label: 'Удалить <sup><10 мин</sup>' },
-    { eventName: 'edit', label: 'Редактировать <sup><30 мин</sup>' },
-  ].filter((action) => {
-    if (action.eventName === 'delete') {
-      return checkIsAvailable(props.deleteTime);
-    }
-    if (action.eventName === 'edit') {
-      return checkIsAvailable(props.editTime);
-    }
-  });
 </script>
 
 <template>
   <div class="flex gap-8">
     <button
       class="hidden text-gray transition-colors hover:text-black tablet:block"
-      v-for="(action, index) in actions"
-      :key="index"
-      @click="emit(action.eventName)"
-      v-html="action.label" />
+      v-if="checkIsAvailable(props.deleteTime)"
+      @click="emit('delete')">
+      Удалить <sup>{{ '<10 мин' }}</sup>
+    </button>
+    <button
+      class="hidden text-gray transition-colors hover:text-black tablet:block"
+      v-if="checkIsAvailable(props.editTime)"
+      @click="emit('edit')">
+      Редактировать <sup>{{ '<30 мин' }}</sup>
+    </button>
     <VFloat
       class="block tablet:hidden"
       v-if="
         checkIsAvailable(props.deleteTime) || checkIsAvailable(props.editTime)
       ">
       <ul>
-        <li class="text-base" v-for="(action, index) in actions" :key="index">
+        <li class="text-base">
           <button
             class="h-32 w-full px-8 text-left text-gray transition-colors hover:text-black"
-            :key="index"
-            @click="emit(action.eventName)"
-            v-html="action.label" />
+            v-if="checkIsAvailable(props.deleteTime)"
+            @click="emit('delete')">
+            Удалить <sup>{{ '<10 мин' }}</sup>
+          </button>
+        </li>
+        <li class="text-base">
+          <button
+            class="h-32 w-full px-8 text-left text-gray transition-colors hover:text-black"
+            v-if="checkIsAvailable(props.editTime)"
+            @click="emit('edit')">
+            Редактировать <sup>{{ '<30 мин' }}</sup>
+          </button>
         </li>
       </ul>
     </VFloat>
