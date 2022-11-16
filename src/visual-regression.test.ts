f0ace6d1489688babc1410779beee65810e9786e
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-11-16 18:06:50 +0100

    blur added

--- src/visual-regression.test.ts
+++ src/visual-regression.test.ts
@@ -15,6 +15,7 @@ const matchConfig = (threshold: number): MatchImageSnapshotOptions => ({
   },
   failureThreshold: threshold,
   failureThresholdType: 'percent',
+  blur: 2,
 });
 
 class VisualTest {
