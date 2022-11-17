3e8e10417d079c61600c3ba7420dcfa3ee31e1d3
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-11-17 13:13:57 +0100

    smaller threshold

--- src/constants/viewports.ts
+++ src/constants/viewports.ts
@@ -9,8 +9,8 @@ export class Viewport {
   }
 }
 
-export const DESKTOP_VIEWPORT = new Viewport(1440, 900, 0.03);
-export const TABLET_VIEWPORT = new Viewport(768, 1024, 0.05);
-export const MOBILE_VIEWPORT = new Viewport(320, 560, 0.08);
+export const DESKTOP_VIEWPORT = new Viewport(1440, 900, 0.01);
+export const TABLET_VIEWPORT = new Viewport(768, 1024, 0.01);
+export const MOBILE_VIEWPORT = new Viewport(320, 560, 0.01);
 
 export const VIEWPORTS = [DESKTOP_VIEWPORT, TABLET_VIEWPORT, MOBILE_VIEWPORT];
