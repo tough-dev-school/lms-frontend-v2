d0b94a210ca40a369867e5178a1ce12a2dedc540
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-11-24 17:12:31 +0100

    tests for VNewAnswer (#96)

---
+++ src/components/VNewAnswer.test.ts
@@ -0,0 +1,80 @@
+import { describe, expect, test, beforeEach, vi } from 'vitest';
+import { mount } from '@vue/test-utils';
+import type { VueWrapper } from '@vue/test-utils';
+import VNewAnswer from '@/components/VNewAnswer.vue';
+import type VTextEditor from '@/components/VTextEditor.vue';
+import type VButton from '@/components/VButton.vue';
+import { faker } from '@faker-js/faker';
+import { createTestingPinia } from '@pinia/testing';
+import useHomework from '@/stores/homework';
+
+const defaultProps = {
+  questionId: faker.datatype.uuid(),
+  parentId: faker.datatype.uuid(),
+};
+
+const text = faker.lorem.sentence();
+
+describe('VNewAnswer', () => {
+  let wrapper: VueWrapper<InstanceType<typeof VNewAnswer>>;
+
+  beforeEach(() => {
+    wrapper = mount(VNewAnswer, {
+      shallow: true,
+      global: {
+        stubs: { VCard: false },
+        plugins: [
+          createTestingPinia({
+            createSpy: vi.fn,
+          }),
+        ],
+      },
+      props: defaultProps,
+    });
+  });
+
+  const getEditorWrapper = () => {
+    return wrapper.findComponent<typeof VTextEditor>('[data-testid="editor"]');
+  };
+
+  const getButtonWrapper = () => {
+    return wrapper.findComponent<typeof VButton>('[data-testid="button"]');
+  };
+
+  test('initially empty', () => {
+    expect(getEditorWrapper().props().modelValue).toBe('');
+  });
+
+  test('sends data on send', async () => {
+    const { parentId, questionId } = defaultProps;
+    const homework = useHomework();
+
+    await getEditorWrapper().vm.$emit('update:modelValue', text);
+    await getButtonWrapper().vm.$emit('click');
+
+    expect(homework.postAnswer).toHaveBeenCalledOnce();
+    expect(homework.postAnswer).toHaveBeenCalledWith({
+      parentId,
+      questionId,
+      text,
+    });
+  });
+
+  test('clears value on send', async () => {
+    await getEditorWrapper().vm.$emit('update:modelValue', text);
+    await getButtonWrapper().vm.$emit('click');
+
+    expect(getEditorWrapper().props().modelValue).toBe(text);
+  });
+
+  test('emits update on send', async () => {
+    await getEditorWrapper().vm.$emit('update:modelValue', text);
+    await getButtonWrapper().vm.$emit('click');
+
+    expect(wrapper.emitted().update).toBeDefined();
+  });
+
+  test('send button is disable with no text', () => {
+    expect(getButtonWrapper().attributes()['disabled']).toBe('true');
+  });
+});
--- src/components/VNewAnswer.vue
+++ src/components/VNewAnswer.vue
@@ -32,12 +32,14 @@
   <VCard class="px-0 pt-0 tablet:px-0">
     <VTextEditor
       v-model="text"
-      class="mb-16 rounded-t border-b border-offwhite" />
+      class="mb-16 rounded-t border-b border-offwhite"
+      data-testid="editor" />
     <div class="flex flex-row-reverse px-32">
       <VButton
         @click="sendPost"
         :disabled="!(text.length > 0)"
         class="max-h-32 min-w-0 px-24"
+        data-testid="button"
         >Отправить</VButton
       >
     </div>
