e8dd322793b99639d81f8a990b48fa48c87ec4ac
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-12-01 14:40:13 +0100

    sentry added (#105)

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,27 @@ import router from './router';
 import FloatingVue from 'floating-vue';
 import 'floating-vue/dist/style.css';
 
+import * as Sentry from '@sentry/vue';
+import { BrowserTracing } from '@sentry/tracing';
+
 import './fonts.css';
 import './tailwind.css';
 
 const app = createApp(App);
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    app,
+    dsn: 'https://4bd3741410104a88a731d82ee59341b0@o47144.ingest.sentry.io/4504038554664960',
+    integrations: [
+      new BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+}
+
 app.use(FloatingVue);
 
 const pinia = createPinia();
