efebcc04f73ff098d02c8a2e4f3e64fa756ceadf
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-12-06 15:27:11 +0100

    tests for VHtmlContent

---
+++ src/components/VHtmlContent.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test, beforeEach } from 'vitest';
+import { mount } from '@vue/test-utils';
+import type { VueWrapper } from '@vue/test-utils';
+import VHtmlContent from './VHtmlContent.vue';
+import { faker } from '@faker-js/faker';
+
+const defaultProps = {
+  content: `<p>${faker.lorem.words(10)}</p>`,
+};
+
+describe('VHtmlContent', () => {
+  let wrapper: VueWrapper<InstanceType<typeof VHtmlContent>>;
+
+  beforeEach(() => {
+    wrapper = mount(VHtmlContent, { shallow: true, props: defaultProps });
+  });
+
+  test('renders html from props', () => {
+    expect(wrapper.html()).toContain(defaultProps.content);
+  });
+});
