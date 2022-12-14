b3e19e1164ed2c713bf041b78bace96bc6ee5625
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-12-14 20:40:52 +0100

    test todos split

---
+++ src/utils/axios/onRequestFullfilled.test.ts
@@ -0,0 +1,12 @@
+import { test, describe } from 'vitest';
+
+describe('onRequestFullfilled', () => {
+  test.todo('request data is converted to snake_case', () => {});
+  test.todo('request data coversion can be disabled', () => {});
+
+  test.todo('auth token added to request headers if exist', () => {});
+  test.todo(
+    'auth token is not added to request headers if not exist',
+    () => {},
+  );
+});
