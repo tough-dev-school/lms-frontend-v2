d8e590e98b6c429db77e4700e13efbcebf615757
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-12-14 20:40:52 +0100

    test todos split

---
+++ src/utils/axios/onResponseFullfilled.test.ts
@@ -0,0 +1,6 @@
+import { test, describe } from 'vitest';
+
+describe('custom axios', () => {
+  test.todo('response data is converted to CamelCase', () => {});
+  test.todo('response data coversion can be disabled', () => {});
+});
--- src/utils/axios/onResponseRejected.test.ts
+++ src/utils/axios/onResponseRejected.test.ts
@@ -4,16 +4,8 @@ describe('custom axios', () => {
   test.todo('response data is converted to CamelCase', () => {});
   test.todo('response data coversion can be disabled', () => {});
 
-  test.todo('request data is converted to snake_case', () => {});
-  test.todo('request data coversion can be disabled', () => {});
-
-  test.todo('auth token added to request headers if exist', () => {});
-  test.todo(
-    'auth token is not added to request headers if not exist',
-    () => {},
-  );
-
   test.todo('logout on 401', () => {});
+
   test.todo('calls handleError with error for json errors', () => {});
   test.todo('calls handleError with error non-json errors', () => {});
 });
