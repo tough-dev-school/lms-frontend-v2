29464e692d13bfe2b3886feb462021d418e1d276
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-12-18 20:51:14 +0100

    dont stretch buttons

--- src/components/VButton.vue
+++ src/components/VButton.vue
@@ -14,7 +14,7 @@
 
 <template>
   <button
-    class="h-module w-full min-w-[240px]"
+    class="h-module min-w-[240px]"
     :class="{
       'leading-1.5 rounded bg-yellow p-module text-center text-black transition-opacity transition-colors hover:bg-yellow-hover disabled:opacity-25':
         tag === 'button',
