ee5bcfd7e808989bb565200e9d1c605629299836
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-12-20 13:29:21 +0100

    better mobile spacing

--- src/components/VCard.vue
+++ src/components/VCard.vue
@@ -12,10 +12,10 @@
 <template>
   <component
     :is="tag"
-    class="overflow-hidden rounded bg-white px-16 py-16 shadow phone:px-32">
+    class="overflow-hidden rounded bg-white p-16 shadow phone:px-32">
     <slot />
     <footer
-      class="mt-32 flex flex-wrap justify-end gap-16 border-t border-gray border-opacity-20 pt-24 pb-16 empty:hidden">
+      class="mt-32 flex flex-wrap justify-end gap-8 border-t border-gray border-opacity-20 pt-16 empty:hidden phone:gap-16 phone:pt-24 phone:pb-16">
       <slot name="footer" />
     </footer>
   </component>
