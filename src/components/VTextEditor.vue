99137630f0a170d4440ff2e44ac30c3eb67c7983
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-12-20 23:06:14 +0100

    Fix text editor input and controls (#147)
    * fix editor input
    
    * placeholder added
    
    * better focus
    
    * bubble menu for style edits
    
    * bubble menu for layout edits
    
    * fix spacing
    
    * single bubble editing
    
    * fix fail on build
    
    * Update screenshots (#148)
    
    Co-authored-by: brachkow <brachkow@users.noreply.github.com>
    
    Co-authored-by: brachkow <brachkow@users.noreply.github.com>

--- src/components/VTextEditor.vue
+++ src/components/VTextEditor.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-  import { EditorContent, Editor } from '@tiptap/vue-3';
+  import Placeholder from '@tiptap/extension-placeholder';
+  import { BubbleMenu, EditorContent, Editor } from '@tiptap/vue-3';
   import StarterKit from '@tiptap/starter-kit';
   import {
     BoldIcon,
@@ -11,13 +12,17 @@
     ListNumbersIcon,
     ListIcon,
   } from 'vue-tabler-icons';
-  import { watch, withDefaults } from 'vue';
+  import { onBeforeUnmount, watch, withDefaults } from 'vue';
 
   export interface Props {
     modelValue: string;
+    placeholder?: string;
   }
 
-  const props = withDefaults(defineProps<Props>(), { modelValue: '' });
+  const props = withDefaults(defineProps<Props>(), {
+    modelValue: '',
+    placeholder: '',
+  });
 
   const emit = defineEmits<{
     (e: 'update:modelValue', value: string): void;
@@ -25,20 +30,23 @@
 
   const editor = new Editor({
     content: props.modelValue,
-    extensions: [StarterKit],
-  });
-
-  editor.on('update', ({ editor }) => {
-    const html = editor.getHTML();
-    emit('update:modelValue', html);
+    extensions: [
+      StarterKit,
+      Placeholder.configure({
+        placeholder: props.placeholder,
+      }),
+    ],
+    onUpdate: () => {
+      const html = editor.getHTML();
+      emit('update:modelValue', html);
+    },
   });
 
   watch(
     props,
     (props) => {
       const { modelValue } = props;
-      const isSame =
-        JSON.stringify(editor.getJSON()) === JSON.stringify(modelValue);
+      const isSame = editor.getHTML() === modelValue;
 
       if (isSame) return;
 
@@ -78,41 +86,98 @@
   const toggleUnorderedList = () => {
     editor.chain().focus().toggleBulletList().run();
   };
+
+  const focus = () => {
+    editor.chain().focus();
+  };
+
+  onBeforeUnmount(() => {
+    editor.destroy();
+  });
 </script>
 
 <template>
   <div class="bg-white">
-    <header class="flex">
-      <button @click="toggleHeading1" class="bubble-button">
-        <H1Icon />
-      </button>
-      <button @click="toggleHeading2" class="bubble-button">
-        <H2Icon />
-      </button>
-      <button @click="toggleHeading3" class="bubble-button">
-        <H3Icon />
-      </button>
-      <button @click="toggleBold" class="bubble-button">
+    <BubbleMenu
+      class="bubble-menu"
+      :editor="editor"
+      :tippy-options="{ duration: 100 }"
+      v-if="editor">
+      <button
+        @click="toggleBold"
+        class="bubble-button"
+        :class="{ 'bubble-button_active': editor.isActive('bold') }">
         <BoldIcon />
       </button>
-      <button @click="toggleItalic" class="bubble-button">
+      <button
+        @click="toggleItalic"
+        class="bubble-button"
+        :class="{ 'bubble-button_active': editor.isActive('italic') }">
         <ItalicIcon />
       </button>
-      <button @click="toggleBlockquote" class="bubble-button">
+      <button
+        @click="toggleHeading1"
+        class="bubble-button"
+        :class="{
+          'bubble-button_active': editor.isActive('heading', { level: 1 }),
+        }">
+        <H1Icon />
+      </button>
+      <button
+        @click="toggleHeading2"
+        class="bubble-button"
+        :class="{
+          'bubble-button_active': editor.isActive('heading', { level: 2 }),
+        }">
+        <H2Icon />
+      </button>
+      <button
+        @click="toggleHeading3"
+        class="bubble-button"
+        :class="{
+          'bubble-button_active': editor.isActive('heading', { level: 3 }),
+        }">
+        <H3Icon />
+      </button>
+      <button
+        @click="toggleBlockquote"
+        class="bubble-button"
+        :class="{ 'bubble-button_active': editor.isActive('blockquote') }">
         <BlockquoteIcon />
       </button>
-      <button @click="toggleOrderedList" class="bubble-button">
+      <button
+        @click="toggleOrderedList"
+        class="bubble-button"
+        :class="{ 'bubble-button_active': editor.isActive('orderedList') }">
         <ListNumbersIcon />
       </button>
-      <button @click="toggleUnorderedList" class="bubble-button">
+      <button
+        @click="toggleUnorderedList"
+        class="bubble-button"
+        :class="{ 'bubble-button_active': editor.isActive('bulletList') }">
         <ListIcon />
       </button>
-    </header>
-    <EditorContent :editor="editor" class="prose max-w-none" />
+    </BubbleMenu>
+    <EditorContent
+      :editor="editor"
+      class="prose max-w-none py-24"
+      @click="focus" />
   </div>
 </template>
 
 <style>
+  .bubble-menu {
+    @apply flex rounded bg-black text-white;
+  }
+
+  .bubble-button {
+    @apply flex h-32 items-center justify-center px-8 first:pl-16 last:pr-16 hover:bg-white hover:bg-opacity-20;
+  }
+
+  .bubble-button_active {
+    @apply bg-white bg-opacity-20;
+  }
+
   .ProseMirror {
     @apply rounded bg-white py-8 px-16 outline-none;
   }
@@ -121,7 +186,8 @@
     @apply focus:outline-none;
   }
 
-  .bubble-button {
-    @apply inline-flex flex-grow items-center justify-center py-4 text-gray transition-colors hover:bg-offwhite hover:bg-opacity-75 hover:text-black;
+  .ProseMirror p.is-editor-empty:first-child::before {
+    content: attr(data-placeholder);
+    @apply pointer-events-none float-left h-0 text-gray;
   }
 </style>
