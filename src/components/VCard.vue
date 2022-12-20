eae20c23b7694f2502f19aa187512fbb07ae372d
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-12-20 13:51:40 +0100

    grow buttons on mobile

--- src/components/VCard.vue
+++ src/components/VCard.vue
@@ -15,8 +15,15 @@
     class="overflow-hidden rounded bg-white p-16 shadow phone:px-32">
     <slot />
     <footer
-      class="mt-32 flex flex-wrap justify-end gap-8 border-t border-gray border-opacity-20 pt-16 empty:hidden phone:gap-16 phone:pt-24 phone:pb-16">
+      class="grow-children mt-32 flex flex-wrap justify-end gap-8 border-t border-gray border-opacity-20 pt-16 empty:hidden phone:gap-16 phone:pt-24 phone:pb-16">
       <slot name="footer" />
     </footer>
   </component>
 </template>
+
+<style>
+  .grow-children > *:not(.flex-grow) {
+    flex-grow: 1;
+    @apply phone:flex-grow-0;
+  }
+</style>
