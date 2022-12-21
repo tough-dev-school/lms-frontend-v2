c1ddd9bd5685da81937db9d5f47b4e95e864b3d8
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-12-21 16:20:18 +0100

    support for hash routing

--- src/router.ts
+++ src/router.ts
@@ -150,6 +150,11 @@ export const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return { el: to.hash, top: 32 };
+    }
+  },
 });
 
 router.beforeEach(async (to: RouteLocationNormalized) => {
