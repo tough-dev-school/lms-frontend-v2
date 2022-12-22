1340330ea820533976620ff9dc57c2cbac555c14
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-12-22 15:53:59 +0100

    «fulfilled» typo fix for tests

--- src/axios/onResponseFullfilled.test.ts
+++
@@ -1,6 +0,0 @@
-import { test, describe } from 'vitest';
-
-describe('custom axios', () => {
-  test.todo('response data is converted to CamelCase', () => {});
-  test.todo('response data coversion can be disabled', () => {});
-});
