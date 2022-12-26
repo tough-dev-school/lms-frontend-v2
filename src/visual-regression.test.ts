f8cbc3789da59dea69f867046ad0baa2e211e3e1
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-12-26 20:24:54 +0100

    VisualTest constructor replaced with interface

--- src/visual-regression.test.ts
+++ src/visual-regression.test.ts
@@ -17,15 +17,10 @@ const matchConfig: MatchImageSnapshotOptions = {
   failureThresholdType: 'pixel',
 };
 
-class VisualTest {
+interface VisualTest {
   name: string;
   path: string;
-  action: () => Promise<void>;
-  constructor(name: string, path: string, action = async () => {}) {
-    this.name = name;
-    this.path = path;
-    this.action = action;
-  }
+  action?: () => Promise<void>;
 }
 
 type colorScheme = 'light' | 'dark';
@@ -42,54 +37,54 @@ describe('visual regression test for', () => {
 
   const tests: Test[] = [];
 
-  const scenarios = [
-    new VisualTest(
-      'Login — email login',
-      `/iframe.html?args=&id=pages-app--login&viewMode=story#/login`,
-    ),
-    new VisualTest(
-      'Login — password login',
-      `/iframe.html?args=&id=pages-app--login&viewMode=story#/login`,
-      async () => {
+  const scenarios: VisualTest[] = [
+    {
+      name: 'Login — email login',
+      path: `/iframe.html?args=&id=pages-app--login&viewMode=story#/login`,
+    },
+    {
+      name: 'Login — password login',
+      path: `/iframe.html?args=&id=pages-app--login&viewMode=story#/login`,
+      action: async () => {
         await page.click('[data-testid="to-password-mode"]');
       },
-    ),
-    new VisualTest(
-      'Login — reset password',
-      `/iframe.html?args=&id=pages-app--reset-password&viewMode=story#/login/reset`,
-    ),
-    new VisualTest(
-      'Login — change password',
-      `/iframe.html?args=&id=pages-app--change-password&viewMode=story#/auth/password/reset/1234567890/1234567890`,
-    ),
-    new VisualTest(
-      'Settings',
-      `/iframe.html?args=&id=pages-app--settings&viewMode=story#/settings`,
-    ),
-    new VisualTest(
-      'Materials',
-      '/iframe.html?args=&id=pages-app--notion-view&viewMode=story#/materials/1234567890',
-    ),
-    new VisualTest(
-      'Materials Missing',
-      '/iframe.html?id=pages-app--notion-view-missing&viewMode=story#/materials/1234567890',
-    ),
-    new VisualTest(
-      'HomeworkAnswers',
-      `/iframe.html?args=&id=pages-app--homework-answer-view&viewMode=story#/homework/answers/1234567890`,
-    ),
-    new VisualTest(
-      'HomeworkExpert',
-      '/iframe.html?args=&id=pages-app--homework-expert-view&viewMode=story#/homework/question-admin/1234567890',
-    ),
-    new VisualTest(
-      'HomeworkQuestion',
-      '/iframe.html?args=&id=pages-app--homework-question-view&viewMode=story#/homework/questions/1234567890',
-    ),
-    new VisualTest(
-      'Shop',
-      '/iframe.html?args=&id=pages-app--shop&viewMode=story#/shop',
-    ),
+    },
+    {
+      name: 'Login — reset password',
+      path: `/iframe.html?args=&id=pages-app--reset-password&viewMode=story#/login/reset`,
+    },
+    {
+      name: 'Login — change password',
+      path: `/iframe.html?args=&id=pages-app--change-password&viewMode=story#/auth/password/reset/1234567890/1234567890`,
+    },
+    {
+      name: 'Settings',
+      path: `/iframe.html?args=&id=pages-app--settings&viewMode=story#/settings`,
+    },
+    {
+      name: 'Materials',
+      path: '/iframe.html?args=&id=pages-app--notion-view&viewMode=story#/materials/1234567890',
+    },
+    {
+      name: 'Materials Missing',
+      path: '/iframe.html?id=pages-app--notion-view-missing&viewMode=story#/materials/1234567890',
+    },
+    {
+      name: 'HomeworkAnswers',
+      path: `/iframe.html?args=&id=pages-app--homework-answer-view&viewMode=story#/homework/answers/1234567890`,
+    },
+    {
+      name: 'HomeworkExpert',
+      path: '/iframe.html?args=&id=pages-app--homework-expert-view&viewMode=story#/homework/question-admin/1234567890',
+    },
+    {
+      name: 'HomeworkQuestion',
+      path: '/iframe.html?args=&id=pages-app--homework-question-view&viewMode=story#/homework/questions/1234567890',
+    },
+    {
+      name: 'Shop',
+      path: '/iframe.html?args=&id=pages-app--shop&viewMode=story#/shop',
+    },
   ];
 
   scenarios.forEach((test) => {
@@ -97,7 +92,7 @@ describe('visual regression test for', () => {
       tests.push([
         `${test.name} — ${viewport.width}×${viewport.height} ${colorScheme}`,
         test.path,
-        test.action,
+        test.action ? test.action : async () => {},
         viewport.width,
         viewport.height,
       ]);
