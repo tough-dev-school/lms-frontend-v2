53ce8f6295daa8eef5d3c45a82e64fdf68dce519
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-12-27 19:51:56 +0100

    layout changes for VHomeView

--- src/views/VHomeView.vue
+++ src/views/VHomeView.vue
@@ -8,10 +8,11 @@
 
 <template>
   <VHeading tag="h1" class="mb-32">Ваши курсы</VHeading>
-  <ul class="flex flex-col gap-16">
+  <ul class="mb-32 flex flex-col gap-16" v-if="studies.items.length > 0">
     <li v-for="(study, index) in studies.items" :key="index">
       <RouterLink
         class="link"
+        data-testid="study"
         :to="{
           name: 'materials',
           params: { id: study.homePageSlug },
@@ -22,13 +23,13 @@
       </RouterLink>
     </li>
   </ul>
-  <div class="mt-32 block text-center">
-    <p v-if="studies.items.length === 0">Нет доступных курсов.</p>
-    <p>
-      Доступ к курсам можно купить на
-      <a href="https://education.borshev.com/" class="link"
-        >education.borshev.com</a
-      >.
-    </p>
-  </div>
+  <p v-else data-testid="empty" class="mb-16 text-center">
+    Нет доступных курсов.
+  </p>
+  <p class="text-center">
+    Доступ к курсам можно купить на
+    <a href="https://education.borshev.com/" class="link"
+      >education.borshev.com</a
+    >.
+  </p>
 </template>
