03cd4c69618295045e4a13f292a9a82591e54de3
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-01-14 22:04:47 +0100

    tests for onRequestFulfilled

--- src/axios/onRequestFulfilled.test.ts
+++ src/axios/onRequestFulfilled.test.ts
@@ -1,12 +1,56 @@
-import { test, describe } from 'vitest';
+import onRequestFulfilled from './onRequestFulfilled';
+import { test, describe, vi, beforeEach, expect } from 'vitest';
+import requestCaseMiddleware from './requestCaseMiddleware';
+import type { AxiosRequestConfig } from 'axios';
+import { faker } from '@faker-js/faker';
+import { createApp } from 'vue';
+import { setActivePinia } from 'pinia';
+import { createTestingPinia } from '@pinia/testing';
+import useAuth from '@/stores/auth';
+
+vi.mock('./requestCaseMiddleware');
+
+const input = { data: {} } as AxiosRequestConfig;
 
 describe('onRequestFulfilled', () => {
-  test.todo('request data is converted to snake_case', () => {});
-  test.todo('request data coversion can be disabled', () => {});
+  let authStore: any;
 
-  test.todo('auth token added to request headers if exist', () => {});
-  test.todo(
-    'auth token is not added to request headers if not exist',
-    () => {},
-  );
+  beforeEach(() => {
+    const app = createApp({});
+    const pinia = createTestingPinia({ createSpy: vi.fn });
+    app.use(pinia);
+    setActivePinia(pinia);
+
+    authStore = useAuth();
+  });
+
+  test('request data is converted to snake_case', () => {
+    onRequestFulfilled(input, true);
+
+    expect(requestCaseMiddleware).toHaveBeenCalledTimes(1);
+    expect(requestCaseMiddleware).toHaveBeenCalledWith(input.data, true);
+  });
+
+  test('request data coversion can be disabled', () => {
+    onRequestFulfilled(input, false);
+
+    expect(requestCaseMiddleware).toHaveBeenCalledTimes(1);
+    expect(requestCaseMiddleware).toHaveBeenCalledWith(input.data, false);
+  });
+
+  test('auth token added to request headers if exist', () => {
+    authStore.token = faker.datatype.uuid();
+
+    const request = onRequestFulfilled(input, true);
+
+    expect(request.headers.Authorization).toBe(`Bearer ${authStore.token}`);
+  });
+
+  test('auth token is not added to request headers if not exist', () => {
+    authStore.token = undefined;
+
+    const request = onRequestFulfilled(input, true);
+
+    expect(request.headers.Authorization).toBe(undefined);
+  });
 });
