641849261cbf8f5954feeeaba86167df5cca3a13
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-02-03 02:51:10 +0100

    fix display of mobile answers

--- src/components/VAnswerActionsDesktop.vue
+++ src/components/VAnswerActionsDesktop.vue
@@ -15,18 +15,20 @@
 </script>
 
 <template>
-  <button
-    class="secondary-action"
-    data-testid="delete"
-    v-if="allowDelete"
-    @click="emit('delete')">
-    Удалить <sup>{{ `<${deleteTime} мин` }}</sup>
-  </button>
-  <button
-    class="secondary-action"
-    data-testid="edit"
-    v-if="allowEdit"
-    @click="emit('edit')">
-    Редактировать <sup>{{ `<${editTime} мин` }}</sup>
-  </button>
+  <div class="flex items-center gap-8">
+    <button
+      class="secondary-action"
+      data-testid="delete"
+      v-if="allowDelete"
+      @click="emit('delete')">
+      Удалить <sup>{{ `<${deleteTime} мин` }}</sup>
+    </button>
+    <button
+      class="secondary-action"
+      data-testid="edit"
+      v-if="allowEdit"
+      @click="emit('edit')">
+      Редактировать <sup>{{ `<${editTime} мин` }}</sup>
+    </button>
+  </div>
 </template>
