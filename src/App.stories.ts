01eb54f4906280593b844126dc2fc8180d7ee01a
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-02-15 15:29:13 +0100

    lint fix

--- src/App.stories.ts
+++ src/App.stories.ts
@@ -23,7 +23,7 @@ import { faker } from '@faker-js/faker';
 import merge from 'lodash/merge';
 import type { Comment } from '@/types/homework';
 import useDiplomas from '@/stores/diplomas';
-import { diplomasData, getDiplomasData } from '@/mocks/diplomas';
+import { diplomasData } from '@/mocks/diplomas';
 
 export default {
   title: 'Pages/App',
