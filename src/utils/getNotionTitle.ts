53c474c9386d466fa2612cc1a6b0df07d2a913f4
Gleb Sakharov <35118149+skhrvg@users.noreply.github.com>
2023-05-23 11:23:03 +0300

    Add fallback value for Notion page title

--- src/utils/getNotionTitle.ts
+++ src/utils/getNotionTitle.ts
@@ -1,12 +1,20 @@
 // @ts-ignore
-import type { BlockMap } from 'vue3-notion/src/lib/types';
+import type { BlockMap, Block } from 'vue3-notion/src/lib/types';
 import idToUuid from './idToUuid';
 
 const getNotionTitle = (materialId: string, material: BlockMap) => {
-  const blockId = idToUuid(materialId);
-
   // as api is unofficial and poorly typed we just assume title value is a nested array and then flatten it with the magic number to convert it into string
-  return material[blockId]?.value?.properties?.title?.flat(100).join('');
+  const getBlockTitle = (block: Block): string | undefined =>
+    block?.value?.properties?.title?.flat(100).join('');
+
+  const blockId = idToUuid(materialId);
+  const firstBlockId = Object.keys(material)[0];
+
+  if (getBlockTitle(material[blockId])) return getBlockTitle(material[blockId]);
+  // using title of the first block as fallback title
+  else if (material[firstBlockId]?.value?.type === 'page')
+    return getBlockTitle(material[firstBlockId]);
+  return undefined;
 };
 
 export default getNotionTitle;
