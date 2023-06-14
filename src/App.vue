7820737ca62bce21e94b79e061dffe523d2840f3
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-06-14 20:18:42 +0200

    Add layout component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
 <script lang="ts" setup>
-  import VProfileMenu from '@/components/VProfileMenu.vue';
-  import VPreloader from '@/components/VPreloader.vue';
-  import VToastFeed from '@/components/VToastFeed.vue';
-  import { useRoute } from 'vue-router';
-  import { computed } from 'vue';
-  import useLoading from '@/stores/loading';
-
-  const route = useRoute();
-  const loading = useLoading();
-
-  const hasHeader = computed(() => {
-    return !route.meta.isPublic;
-  });
+  import VDefaultLayout from '@/layouts/VDefaultLayout.vue';
 </script>
 
 <template>
-  <main class="container pb-128" v-if="!loading.isLoading">
-    <header class="flex justify-end pt-16 pb-24 tablet:pb-64" v-if="hasHeader">
-      <VProfileMenu class="w-full tablet:w-auto" />
-    </header>
-    <RouterView />
-  </main>
-  <VPreloader v-else />
-  <VToastFeed />
+  <VDefaultLayout><RouterView /></VDefaultLayout>
 </template>
