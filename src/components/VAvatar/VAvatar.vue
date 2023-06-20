ec297f66f10cb35caa017bf3c0f93996c6079841
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-06-20 13:48:48 +0200

    Remove doc comment

--- src/components/VAvatar/VAvatar.vue
+++ src/components/VAvatar/VAvatar.vue
@@ -1,14 +1,3 @@
-<!-- import { minidenticon } from 'minidenticons'
-import { useMemo } from 'react'
-
-const MinidenticonImg = ({ username, saturation, lightness, ...props }) => {
-  const svgURI = useMemo(
-    () => 'data:image/svg+xml;utf8,' + encodeURIComponent(minidenticon(username, saturation, lightness)),
-    [username, saturation, lightness]
-  )
-  return (<img src={svgURI} alt={username} {...props} />)
-} -->
-
 <script lang="ts" setup>
   import { minidenticon } from 'minidenticons';
   import { computed } from 'vue';
