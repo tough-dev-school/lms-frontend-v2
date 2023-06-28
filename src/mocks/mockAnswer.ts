09121981390e2e5a313c4071555ff0536956999d
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-06-29 01:39:22 +0200

    Remove reactions from default mock

--- src/mocks/mockAnswer.ts
+++ src/mocks/mockAnswer.ts
@@ -2,7 +2,6 @@ import type { Answer } from '@/types/homework';
 import dayjs from 'dayjs';
 import { faker } from '@faker-js/faker';
 import { mockAuthor } from './mockAuthor';
-import { mockReaction } from './mockReaction';
 import htmlToMarkdown from '@/utils/htmlToMarkdown';
 import { LOREM_CONTENT, mockContent } from './mockContent';
 
@@ -17,9 +16,7 @@ export const mockAnswer = (payload: Partial<Answer> = {}): Answer => {
     text,
     src: htmlToMarkdown(text),
     hasDescendants: false,
-    reactions: faker.helpers.multiple(mockReaction, {
-      count: { min: 0, max: 5 },
-    }),
+    reactions: [],
     ...payload,
   };
 };
