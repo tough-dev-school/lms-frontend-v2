7239e1fd99a622c32369f76537cb84e4bca49274
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-07-12 14:27:35 +0200

    «Продам гараж»

--- src/layouts/VDefaultLayout/VDefaultLayout.vue
+++ src/layouts/VDefaultLayout/VDefaultLayout.vue
@@ -16,6 +16,14 @@
 
 <template>
   <main class="container pb-128" v-if="!loading.isLoading">
+    <p class="p-8 px-16 bg-lightgray text-black rounded mt-16">
+      Это тестовая ветка с новым рендером ноушена. О багах писать
+      <a
+        class="link"
+        href="https://3.basecamp.com/5104612/buckets/29069198/todolists/6338720082"
+        >сюда</a
+      >.
+    </p>
     <header class="flex justify-end pt-16 pb-24 tablet:pb-64" v-if="hasHeader">
       <VProfileMenu class="w-full tablet:w-auto" />
     </header>
