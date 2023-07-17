3558fd873075e10b046f2462a16c52f33deff6f2
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-07-17 12:44:50 +0200

    Revert "Remove all buttons"
    This reverts commit 94c7bd59d0768b5ef7ccb5c683890a7a32714811.

--- src/views/VMailSentView/VMailSentView.vue
+++ src/views/VMailSentView/VMailSentView.vue
@@ -1,17 +1,32 @@
 <script lang="ts" setup>
   import { VHeading } from '@/components/VHeading';
   import { VCard } from '@/components/VCard';
-  import { useRoute } from 'vue-router';
+  import { VButton } from '@/components/VButton';
+  import { useRouter, useRoute } from 'vue-router';
   import { computed } from 'vue';
 
+  const router = useRouter();
   const route = useRoute();
 
   const email = computed(() => String(route.query.email));
+
+  const back = () => {
+    router.push({ name: 'login' });
+  };
 </script>
 
 <template>
   <VCard class="mt-[25vh] flex flex-col gap-16">
     <VHeading tag="h1">Ссылка отправлена</VHeading>
-    <p data-testid="message">Мы отправили ссылку для входа на {{ email }}.</p>
+    <p data-testid="message">Мы отправили ссылку по адресу {{ email }}</p>
+    <template #footer>
+      <VButton
+        class="flex-grow"
+        appearance="link"
+        @click="back"
+        data-testid="restart"
+        >На другую почту</VButton
+      >
+    </template>
   </VCard>
 </template>
