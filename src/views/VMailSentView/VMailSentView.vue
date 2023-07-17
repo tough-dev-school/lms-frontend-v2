223ee02354f06dee26ae644d1bb4102ed74f01d9
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-07-17 13:05:57 +0200

    Quick open popular email clients

--- src/views/VMailSentView/VMailSentView.vue
+++ src/views/VMailSentView/VMailSentView.vue
@@ -1,31 +1,38 @@
 <script lang="ts" setup>
   import { VHeading } from '@/components/VHeading';
   import { VCard } from '@/components/VCard';
-  import { VButton } from '@/components/VButton';
-  import { useRouter, useRoute } from 'vue-router';
+  import { useRoute } from 'vue-router';
   import { computed } from 'vue';
 
-  const router = useRouter();
   const route = useRoute();
 
   const email = computed(() => String(route.query.email));
 
-  const back = () => {
-    router.push({ name: 'login' });
-  };
+  interface EmailProvider {
+    label: string;
+    url: string;
+    keyword: string;
+  }
+
+  const KNOWN_EMAIL_PROVIDERS: EmailProvider[] = [
+    { label: 'GMail', url: 'https://mail.google.com/', keyword: 'gmail.com' },
+    { label: 'Mail.ru', url: 'https://e.mail.ru/inbox/', keyword: 'mail.ru' },
+  ];
+
+  const emailProvider = computed(() => {
+    return KNOWN_EMAIL_PROVIDERS.find((provider) => {
+      return email.value.includes(provider.keyword);
+    });
+  });
 </script>
 
 <template>
-  <VCard class="mt-[25vh] flex flex-col gap-16">
+  <VCard class="flex flex-col gap-16">
     <VHeading tag="h1">Ссылка отправлена</VHeading>
     <p data-testid="message">Мы отправили ссылку по адресу {{ email }}</p>
     <template #footer>
-      <VButton
-        class="flex-grow"
-        appearance="link"
-        @click="back"
-        data-testid="restart"
-        >На другую почту</VButton
+      <a class="flex-grow button" :href="emailProvider.url" v-if="emailProvider"
+        >Открыть {{ emailProvider.label }}</a
       >
     </template>
   </VCard>
