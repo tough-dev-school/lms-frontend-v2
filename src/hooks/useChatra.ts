e645882b07e2a2695637dd886b7df10a4333829a
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-07-18 14:58:15 +0200

    Fix type error

--- src/hooks/useChatra.ts
+++ src/hooks/useChatra.ts
@@ -1,10 +1,12 @@
 import { onMounted, ref } from 'vue';
 
+type CustomWindow = typeof window & { Chatra: any };
+
 export const useChatra = () => {
   const chatra: any = ref(undefined);
 
   onMounted(() => {
-    chatra.value = window.Chatra;
+    chatra.value = (window as CustomWindow).Chatra;
   });
 
   return { chatra };
