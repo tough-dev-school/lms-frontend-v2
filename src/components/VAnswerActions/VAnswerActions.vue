561a4dd9668e068c831f6ed2d209bf57d5e2be53
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-07-21 13:23:51 +0200

    New answer editing flow (#265)
    * New editing flow
    
    * Don't compute hasDescendants
    
    * Remove props from answer actions
    
    * Add hasDescendants flag to story
    
    * Invert isEditable
    
    * Update screenshots
    
    * Remove not needed tests
    
    * Fix hasDescendants detection
    
    * Better types for Answer
    
    * Remove fixes as unneeded due to backend update
    
    ---------
    
    Co-authored-by: brachkow <brachkow@users.noreply.github.com>

--- src/components/VAnswerActions/VAnswerActions.vue
+++ src/components/VAnswerActions/VAnswerActions.vue
@@ -1,47 +1,22 @@
 <script lang="ts" setup>
-  import dayjs from 'dayjs';
-  import { computed } from 'vue';
   import { VAnswerActionsDesktop } from '@/components/VAnswerActionsDesktop';
   import { VAnswerActionsMobile } from '@/components/VAnswerActionsMobile';
-
-  export interface Props {
-    created: string;
-    deleteTime: number;
-    editTime: number;
-  }
-
-  const props = defineProps<Props>();
 
   const emit = defineEmits<{
     delete: [];
     edit: [];
   }>();
-
-  const checkIsAvailable = (timeout: number) => {
-    return +dayjs() < +dayjs(props.created).add(timeout, 'minutes');
-  };
-
-  const allowDelete = computed(() => checkIsAvailable(props.deleteTime));
-  const allowEdit = computed(() => checkIsAvailable(props.editTime));
 </script>
 
 <template>
   <div class="flex gap-8">
     <VAnswerActionsDesktop
       class="hidden tablet:flex"
-      :deleteTime="deleteTime"
-      :editTime="editTime"
-      :allowDelete="allowDelete"
-      :allowEdit="allowEdit"
       data-testid="desktop"
       @edit="emit('edit')"
       @delete="emit('delete')" />
     <VAnswerActionsMobile
       class="tablet:hidden"
-      :deleteTime="deleteTime"
-      :editTime="editTime"
-      :allowDelete="allowDelete"
-      :allowEdit="allowEdit"
       data-testid="mobile"
       @edit="emit('edit')"
       @delete="emit('delete')" />
