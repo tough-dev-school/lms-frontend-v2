1e6ba870b00bde59fb4999a7de9bdf128998fed5
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-11-10 15:12:21 +0100

    Switch darkmode class

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <script lang="ts" setup>
   import VDefaultLayout from '@/layouts/VDefaultLayout/VDefaultLayout.vue';
+  import { usePreferredDark } from '@vueuse/core';
+  import { computed, watch } from 'vue';
+  import usePreferencesStore, { DarkMode } from '@/stores/preferences';
+
+  const isPreferredDark = usePreferredDark();
+  const preferencesStore = usePreferencesStore();
+
+  const isDark = computed(() => {
+    if (preferencesStore.darkMode === DarkMode.System)
+      return isPreferredDark.value;
+    else if (preferencesStore.darkMode === DarkMode.Light) return false;
+    else if (preferencesStore.darkMode === DarkMode.Dark) return true;
+    else return false;
+  });
 </script>
 
 <template>
-  <VDefaultLayout><RouterView /></VDefaultLayout>
+  <div :class="{ dark: isDark }">
+    <VDefaultLayout>
+      <div class="dark:block hidden">It is darkmode</div>
+      <RouterView />
+    </VDefaultLayout>
+  </div>
 </template>
