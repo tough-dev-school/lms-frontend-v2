a3c31f83ff96e60bb3f57141762304ff96bb9727
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-11-10 15:12:27 +0100

    Improve styles

--- src/components/VPreferencesSettings/VPreferencesSettings.vue
+++ src/components/VPreferencesSettings/VPreferencesSettings.vue
@@ -3,6 +3,7 @@
   import VCard from '@/components/VCard/VCard.vue';
   import usePreferencesStore, { DarkMode } from '@/stores/preferences';
   import { watch, ref, onMounted } from 'vue';
+  import { MoonFilledIcon, DevicesIcon, SunFilledIcon } from 'vue-tabler-icons';
 
   const preferencesStore = usePreferencesStore();
 
@@ -26,30 +27,55 @@
     <!-- # TODO Add unified input wrapper-->
     <div class="flex flex-wrap gap-16">
       <div class="w-full">Темная тема</div>
-      <label>
-        <input
-          v-model="darkMode"
-          type="radio"
-          name="darkMode"
-          :value="DarkMode.System" />
-        Как в системе
-      </label>
-      <label>
-        <input
-          v-model="darkMode"
-          type="radio"
-          name="darkMode"
-          :value="DarkMode.Light" />
-        Светлая
-      </label>
-      <label>
-        <input
-          v-model="darkMode"
-          type="radio"
-          name="darkMode"
-          :value="DarkMode.Dark" />
-        Темная
-      </label>
+      <div class="flex">
+        <label
+          class="Radiobutton"
+          :class="{ Radiobutton_Active: darkMode === DarkMode.System }">
+          <input
+            v-model="darkMode"
+            type="radio"
+            name="darkMode"
+            class="visually-hidden"
+            :value="DarkMode.System" />
+          <DevicesIcon />
+          Как в системе
+        </label>
+        <label
+          class="Radiobutton"
+          :class="{ Radiobutton_Active: darkMode === DarkMode.Light }">
+          <input
+            v-model="darkMode"
+            type="radio"
+            name="darkMode"
+            class="visually-hidden"
+            :value="DarkMode.Light" />
+          <SunFilledIcon />
+          Светлая
+        </label>
+        <label
+          class="Radiobutton"
+          :class="{ Radiobutton_Active: darkMode === DarkMode.Dark }">
+          <input
+            v-model="darkMode"
+            type="radio"
+            name="darkMode"
+            class="visually-hidden"
+            :value="DarkMode.Dark" />
+          <MoonFilledIcon />
+          Темная
+        </label>
+      </div>
     </div>
   </VCard>
 </template>
+
+<style scoped>
+  .Radiobutton {
+    @apply border-y px-8 py-8 gap-4 flex border-gray bg-offwhite cursor-pointer transition-colors;
+    @apply first:rounded-l-4 first:border-l last:rounded-r-4 last:border-r;
+  }
+
+  .Radiobutton_Active {
+    @apply bg-yellow;
+  }
+</style>
