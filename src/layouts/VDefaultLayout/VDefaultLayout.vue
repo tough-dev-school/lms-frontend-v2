1f7b5b0428bae3c7662ace51f8bb5c2332001e57
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-11-14 19:51:09 +0100

    Revert header changes

--- src/layouts/VDefaultLayout/VDefaultLayout.vue
+++ src/layouts/VDefaultLayout/VDefaultLayout.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-  // #TODO Remove test message when new-notion is merged
-
   import VProfileMenu from '@/components/VProfileMenu/VProfileMenu.vue';
   import VPreloader from '@/components/VPreloader/VPreloader.vue';
   import VToastFeed from '@/components/VToastFeed/VToastFeed.vue';
@@ -11,29 +9,22 @@
   const route = useRoute();
   const loading = useLoading();
 
-  const hasHeader = computed(() => {
+  const hasProfile = computed(() => {
     return !route.meta.isPublic;
   });
-
-  const isNotionTest = computed(() =>
-    window.location.host.includes('new-notion'),
-  );
 </script>
 
 <template>
   <main v-if="!loading.isLoading" class="container pb-128">
-    <p
-      v-if="isNotionTest"
-      class="p-8 px-16 bg-lightgray text-black rounded mt-16">
-      Это тестовая ветка с новым рендером ноушена. О багах писать
-      <a
-        class="link"
-        href="https://3.basecamp.com/5104612/buckets/29069198/todolists/6338720082"
-        >сюда</a
-      >.
-    </p>
-    <header v-if="hasHeader" class="flex justify-end pt-16 pb-24 tablet:pb-64">
-      <VProfileMenu class="w-full tablet:w-auto" />
+    <header
+      class="flex flex-wrap justify-between items-center pt-16 pb-24 tablet:pb-64 gap-8">
+      <RouterLink to="/">
+        <div class="flex gap-8 p-8 items-end">
+          <img src="/logo.svg" class="h-32" />
+          <img src="/logo-text.svg" class="h-[28px]" />
+        </div>
+      </RouterLink>
+      <VProfileMenu v-if="hasProfile" class="tablet:w-auto w-full" />
     </header>
     <slot />
   </main>
