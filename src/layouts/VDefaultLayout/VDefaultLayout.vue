01534a8daf3182be677023d2a09b3623276ab407
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-11-28 20:32:17 +0100

    Add darkmode logo

--- src/layouts/VDefaultLayout/VDefaultLayout.vue
+++ src/layouts/VDefaultLayout/VDefaultLayout.vue
@@ -2,6 +2,7 @@
   import VProfileMenu from '@/components/VProfileMenu/VProfileMenu.vue';
   import VPreloader from '@/components/VPreloader/VPreloader.vue';
   import VToastFeed from '@/components/VToastFeed/VToastFeed.vue';
+  import VLogo from '@/components/VLogo/VLogo.vue';
   import { useRoute } from 'vue-router';
   import { computed } from 'vue';
   import useLoading from '@/stores/loading';
@@ -18,12 +19,7 @@
   <main v-if="!loading.isLoading" class="container-fluid pb-128">
     <header
       class="flex flex-wrap justify-between items-center pt-16 pb-24 tablet:pb-64 gap-8">
-      <RouterLink to="/">
-        <div class="flex gap-8 p-8 items-end">
-          <img src="/logo.svg" class="h-32" />
-          <img src="/logo-text.svg" class="h-[28px]" />
-        </div>
-      </RouterLink>
+      <VLogo />
       <VProfileMenu v-if="hasProfile" class="tablet:w-auto w-full" />
     </header>
     <slot />
