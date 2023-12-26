36740226f50fbe60b8bb553e97f9f6c1eb6e63d1
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-12-26 19:09:38 +0100

    Add VHeader tests

--- src/components/VHeader/VHeader.test.ts
+++ src/components/VHeader/VHeader.test.ts
@@ -1,11 +1,48 @@
 import { mount, VueWrapper } from '@vue/test-utils';
 import { faker } from '@faker-js/faker';
 import VHeader from './VHeader.vue';
+import { useRoute } from 'vue-router';
+
+vi.mock('vue-router');
 
 describe('VHeader', () => {
   let wrapper: VueWrapper<InstanceType<typeof VHeader>>;
 
-  beforeEach(() => {
+  const mountComponent = () => {
     wrapper = mount(VHeader, { shallow: true });
+  };
+
+  beforeEach(() => {
+    (useRoute as ReturnType<typeof vi.fn>).mockReturnValue({
+      meta: { isPublic: faker.datatype.boolean() },
+    });
+
+    mountComponent();
+  });
+
+  const getLogoWrapper = () => wrapper.find('[data-testid="logo"]');
+  const getProfileMenuWrapper = () => wrapper.find('[data-testid="profile"]');
+
+  test('has logo that leads to home page', () => {
+    expect(getLogoWrapper().exists()).toBe(true);
+    expect(getLogoWrapper().attributes('to')).toBe('/');
+  });
+
+  test('has profile menu if route is private', () => {
+    (useRoute as ReturnType<typeof vi.fn>).mockReturnValue({
+      meta: { isPublic: false },
+    });
+    mountComponent();
+
+    expect(getProfileMenuWrapper().exists()).toBe(true);
+  });
+
+  test('has no profile menu if route is public', () => {
+    (useRoute as ReturnType<typeof vi.fn>).mockReturnValue({
+      meta: { isPublic: true },
+    });
+    mountComponent();
+
+    expect(getProfileMenuWrapper().exists()).toBe(false);
   });
 });
--- src/components/VHeader/VHeader.vue
+++ src/components/VHeader/VHeader.vue
@@ -13,12 +13,12 @@
 <template>
   <header
     class="flex flex-wrap justify-between items-center pt-16 pb-24 tablet:pb-64 gap-8">
-    <RouterLink to="/">
-      <div class="se:flex hidden gap-8 p-8 items-end">
+    <RouterLink to="/" data-testid="logo" class="se:block hidden">
+      <div class="flex gap-8 p-8 items-end">
         <img src="/logo.svg" class="h-32" />
         <img src="/logo-text.svg" class="h-[28px] phone:block hidden" />
       </div>
     </RouterLink>
-    <VProfileMenu v-if="hasProfile" />
+    <VProfileMenu v-if="hasProfile" data-testid="profile" />
   </header>
 </template>
