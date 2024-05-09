96b8871a1071ef742b0cc3e953350e748fcafe79
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2024-05-09 15:22:33 +0200

    Fix cors middleware

--- functions/_middleware.ts
+++
@@ -1,8 +0,0 @@
-import { PagesFunction } from '@cloudflare/workers-types';
-
-export const onRequest: PagesFunction = async ({ next }) => {
-  const response = await next();
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Max-Age', '86400');
-  return response;
-};
