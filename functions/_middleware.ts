b672f25ca3703ff00f9b243bab3d4c8f89879606
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2024-05-20 14:40:59 +0200

    Test if middleware is working

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -2,20 +2,12 @@
 import { type PagesFunction } from '@cloudflare/workers-types';
 
 export const onRequestOptions: PagesFunction = async () => {
-  return new Response(null, {
-    status: 204,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': '*',
-      'Access-Control-Allow-Methods': 'GET, OPTIONS',
-      'Access-Control-Max-Age': '86400',
-    },
-  });
+  return new Response('Hello from options middleware.ts!');
 };
 
 export const onRequest: PagesFunction = async ({ next }) => {
-  const response = await next();
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Max-Age', '86400');
-  return response;
+  // const response = await next();
+  // response.headers.set('Access-Control-Allow-Origin', '*');
+  // response.headers.set('Access-Control-Max-Age', '86400');
+  return new Response('Hello from middleware.ts!');
 };
