d64f559764bd7ea70f9230c11d6f73668f7b28e9
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2024-05-20 14:33:20 +0200

    Try (insecure) middleware

---
+++ functions/_middleware.ts
@@ -0,0 +1,20 @@
+import { type PagesFunction } from '@cloudflare/workers-types';
+
+export const onRequestOptions: PagesFunction = async () => {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Max-Age': '86400',
+    },
+  });
+};
+
+export const onRequest: PagesFunction = async ({ next }) => {
+  const response = await next();
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Max-Age', '86400');
+  return response;
+};
