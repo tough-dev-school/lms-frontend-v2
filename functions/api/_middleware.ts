72b1832ef2a3ce8a0c52acfb303d80391d89973d
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2024-05-20 15:10:24 +0200

    Return real middleware

--- functions/api/_middleware.ts
+++ functions/api/_middleware.ts
@@ -3,25 +3,21 @@ import { PagesFunction } from '@cloudflare/workers-types';
 
 // const API_URL = 'https://app.tough-dev.school';
 
-// export const onRequestOptions: PagesFunction = async () => {
-//   return new Response(null, {
-//     status: 204,
-//     headers: {
-//       'Access-Control-Allow-Origin': '*',
-//       'Access-Control-Allow-Headers': '*',
-//       'Access-Control-Allow-Methods': 'GET, OPTIONS',
-//       'Access-Control-Max-Age': '86400',
-//     },
-//   });
-// };
+export const onRequestOptions: PagesFunction = async () => {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Max-Age': '86400',
+    },
+  });
+};
 
-// export const onRequest: PagesFunction = async ({ next }) => {
-//   const response = await next();
-//   response.headers.set('Access-Control-Allow-Origin', '*');
-//   response.headers.set('Access-Control-Max-Age', '86400');
-//   return response;
-// };
-
-export const onRequest: PagesFunction = () => {
-  return new Response('Hello form middleware!');
+export const onRequest: PagesFunction = async ({ next }) => {
+  const response = await next();
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Max-Age', '86400');
+  return response;
 };
