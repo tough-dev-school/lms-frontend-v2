7d27c601bdc4858b28996cfdc46be55d8e325d23
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2024-05-21 11:23:57 +0200

    Try to limit allowed origins

--- functions/api/_middleware.ts
+++ functions/api/_middleware.ts
@@ -2,12 +2,18 @@
 import { PagesFunction } from '@cloudflare/workers-types';
 
 const API_URL = 'https://app.tough-dev.school';
+const ALLOWED_ORIGIN = 'lms-frontend-v2.pages.dev';
 
-export const onRequestOptions: PagesFunction = async () => {
+const getAllowedOrigin = (url: string) => {
+  const hostname = new URL(url).hostname;
+  hostname.endsWith(ALLOWED_ORIGIN) ? hostname : '';
+};
+
+export const onRequestOptions: PagesFunction = async (context) => {
   return new Response(null, {
     status: 204,
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': getAllowedOrigin(context.request.url),
       'Access-Control-Allow-Headers': '*',
       'Access-Control-Allow-Methods': 'GET, OPTIONS',
       'Access-Control-Max-Age': '86400',
@@ -24,7 +30,10 @@ export const onRequest: PagesFunction = async (context) => {
 
   const response = new Response(immutableResponse.body, immutableResponse);
 
-  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set(
+    'Access-Control-Allow-Origin',
+    getAllowedOrigin(context.request.url),
+  );
   response.headers.set('Access-Control-Max-Age', '86400');
   return response;
 };
