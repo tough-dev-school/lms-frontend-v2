f2b5704de401440f59d06feec8453c3412ec5d29
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2024-05-23 16:28:30 +0200

    Set wildcard origin if origin is allowed

--- functions/api/_middleware.ts
+++ functions/api/_middleware.ts
@@ -4,23 +4,25 @@ import { PagesFunction } from '@cloudflare/workers-types';
 const API_URL = 'https://app.tough-dev.school';
 const ALLOWED_ORIGIN = 'lms-frontend-v2.pages.dev';
 
-const getAllowedOrigin = (url: string) => {
-  const hostname = new URL(url).hostname;
-  return hostname.endsWith(ALLOWED_ORIGIN)
-    ? hostname
-    : `https://${ALLOWED_ORIGIN}`;
+const isAllowedOrigin = (url: string) => {
+  return new URL(url).hostname.endsWith(ALLOWED_ORIGIN);
 };
 
 export const onRequestOptions: PagesFunction = async (context) => {
-  return new Response(null, {
+  const response = new Response(null, {
     status: 204,
     headers: {
-      'Access-Control-Allow-Origin': getAllowedOrigin(context.request.url),
       'Access-Control-Allow-Headers': '*',
       'Access-Control-Allow-Methods': 'GET, OPTIONS',
       'Access-Control-Max-Age': '86400',
     },
   });
+
+  if (isAllowedOrigin(context.request.url)) {
+    response.headers.set('Access-Control-Allow-Origin', '*');
+  }
+
+  return response;
 };
 
 export const onRequest: PagesFunction = async (context) => {
@@ -29,13 +31,12 @@ export const onRequest: PagesFunction = async (context) => {
   const immutableResponse = await fetch(
     new Request(targetUrl, context.request),
   );
-
   const response = new Response(immutableResponse.body, immutableResponse);
 
-  response.headers.set(
-    'Access-Control-Allow-Origin',
-    getAllowedOrigin(context.request.url),
-  );
-  response.headers.set('Access-Control-Max-Age', '86400');
+  if (isAllowedOrigin(context.request.url)) {
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Max-Age', '86400');
+  }
+
   return response;
 };
