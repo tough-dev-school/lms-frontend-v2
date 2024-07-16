63f9463aee9f04f9d5a48694ef11758a3f1a8301
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2024-07-16 21:43:43 +0300

    fix

--- src/components/VCrossChecks/VCrossChecks.vue
+++ src/components/VCrossChecks/VCrossChecks.vue
@@ -17,23 +17,28 @@
   <div
     v-if="notCheckedCrossChecks.length > 0"
     class="overflow-hidden rounded bg-white p-16 shadow dark:bg-dark-gray phone:px-24 tablet:px-32 bg-yellow-light">
-    <p class="mb-16 font-bold">
-      Вы не сможете посмотреть проверки вашей домашки, пока не проверите работы
-      коллег
-    </p>
     <p class="mb-8">Выберите работу коллеги по курсу для проверки:</p>
-    <ul>
+    <ul class="mb-16">
       <li
         v-for="crosscheck in notCheckedCrossChecks"
         :key="crosscheck.answer.url"
         data-testid="crosscheck">
-        <router-link class="link" :to="crosscheck.answer.url">{{
-          getName(
-            crosscheck.answer.author.firstName,
-            crosscheck.answer.author.lastName,
-          )
-        }}</router-link>
+        <router-link class="link" :to="crosscheck.answer.url"
+          >{{
+            getName(
+              crosscheck.answer.author.firstName,
+              crosscheck.answer.author.lastName,
+            )
+          }}
+          ({{
+            crosscheck.is_checked ? 'проверено' : 'не проверено'
+          }})</router-link
+        >
       </li>
     </ul>
+    <p class="font-bold">
+      Вы не сможете посмотреть проверки вашей домашки, пока не проверите работы
+      коллег
+    </p>
   </div>
 </template>
