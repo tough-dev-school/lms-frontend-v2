0c600216a77ccb65799bfa8e4cb9673df218876a
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-04-06 14:51:16 +0200

    Update title

--- src/views/VHomeView/VHomeView.vue
+++ src/views/VHomeView/VHomeView.vue
@@ -9,7 +9,7 @@
 </script>
 
 <template>
-  <VHeading tag="h1" class="mb-32">Ваши курсы</VHeading>
+  <VHeading tag="h1" class="mb-32">Мои курсы</VHeading>
   <ul v-if="studies.items.length > 0" class="mb-32 flex flex-col gap-16">
     <li v-for="(study, index) in studies.items" :key="index">
       <RouterLink
