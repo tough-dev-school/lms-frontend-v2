2630e5ae41892cedf554df6e9e5dd13cfd940e5f
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-04-12 20:19:48 +0200

    Fix type errors

--- src/views/VMailSentView/VMailSentView.test.ts
+++ src/views/VMailSentView/VMailSentView.test.ts
@@ -1,4 +1,4 @@
-import { vi, describe, beforeEach, expect, test } from 'vitest';
+import { vi, describe, beforeEach, expect, test, afterEach } from 'vitest';
 import { mount, VueWrapper } from '@vue/test-utils';
 import { createTestingPinia } from '@pinia/testing';
 import { faker } from '@faker-js/faker';
