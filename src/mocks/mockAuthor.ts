a9fc358f03d3cc98bc3256da6a38e52d9ffaf6b7
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-04-27 18:32:40 +0200

    Better mock

--- src/mocks/mockAuthor.ts
+++ src/mocks/mockAuthor.ts
@@ -1,24 +1,25 @@
 import type { Author } from '@/types/homework';
 import { fakerRU as faker } from '@faker-js/faker';
 
-export const mockAuthor = (payload: Partial<Author> = {}): Author => ({
-  uuid: faker.string.uuid(),
-  firstName: faker.person.firstName(),
-  lastName: faker.person.lastName(),
-  ...payload,
-});
+export const mockAuthor = ({
+  seed,
+  payload,
+}: {
+  seed?: number;
+  payload?: Partial<Author>;
+} = {}) => {
+  if (seed) faker.seed(seed);
 
-const staticAuthor = (seed: number) => {
-  faker.seed(seed);
   return {
     uuid: faker.string.uuid(),
     firstName: faker.person.firstName(),
     lastName: faker.person.lastName(),
+    ...payload,
   };
 };
 
-export const STATIC_AUTHOR_1: Author = staticAuthor(1);
+export const STATIC_AUTHOR_1: Author = mockAuthor({ seed: 1 });
 
-export const STATIC_AUTHOR_2: Author = staticAuthor(2);
+export const STATIC_AUTHOR_2: Author = mockAuthor({ seed: 2 });
 
-export const STATIC_AUTHOR_3: Author = staticAuthor(3);
+export const STATIC_AUTHOR_3: Author = mockAuthor({ seed: 3 });
