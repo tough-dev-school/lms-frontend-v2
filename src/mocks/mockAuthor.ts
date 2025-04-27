d7d7905c327c4473e1eaa414c5a77427f4ad4245
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-04-27 18:27:48 +0200

    Try to use other way to static mocks

--- src/mocks/mockAuthor.ts
+++ src/mocks/mockAuthor.ts
@@ -1,6 +1,5 @@
 import type { Author } from '@/types/homework';
-import { faker } from '@faker-js/faker';
-import { USER_1, USER_2, USER_3 } from './mockUserId';
+import { fakerRU as faker } from '@faker-js/faker';
 
 export const mockAuthor = (payload: Partial<Author> = {}): Author => ({
   uuid: faker.string.uuid(),
@@ -9,20 +8,17 @@ export const mockAuthor = (payload: Partial<Author> = {}): Author => ({
   ...payload,
 });
 
-export const STATIC_AUTHOR_1: Author = {
-  uuid: USER_1,
-  firstName: 'Янка',
-  lastName: 'Купала',
+const staticAuthor = (seed: number) => {
+  faker.seed(seed);
+  return {
+    uuid: faker.string.uuid(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+  };
 };
 
-export const STATIC_AUTHOR_2: Author = {
-  uuid: USER_2,
-  firstName: 'Якуб',
-  lastName: 'Колас',
-};
+export const STATIC_AUTHOR_1: Author = staticAuthor(1);
 
-export const STATIC_AUTHOR_3: Author = {
-  uuid: USER_3,
-  firstName: 'Максим',
-  lastName: 'Богданович',
-};
+export const STATIC_AUTHOR_2: Author = staticAuthor(1);
+
+export const STATIC_AUTHOR_3: Author = staticAuthor(1);
