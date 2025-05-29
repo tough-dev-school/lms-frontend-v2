ad5139fa430e3c6921df7f6231d2972d7c1bc4e1
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-05-29 08:44:42 +0200

    Fix answer editing (#359)

--- src/components/VExistingAnswer/VExistingAnswerProvider.vue
+++ src/components/VExistingAnswer/VExistingAnswerProvider.vue
@@ -1,15 +1,16 @@
 <script lang="ts" setup>
   import VExistingAnswer from './VExistingAnswer.vue';
-  import { useHomeworkAnswerQuery } from '@/query';
+  import { useHomeworkAnswerQuery, useUserQuery } from '@/query';
 
   const props = defineProps<{
     answerId: string;
   }>();
 
   const { data: answer } = useHomeworkAnswerQuery(() => props.answerId);
+  const { data: user } = useUserQuery();
 </script>
 
 <template>
-  <VExistingAnswer v-if="answer" :answer="answer" />
+  <VExistingAnswer v-if="answer && user" :answer="answer" :user="user" />
   <div v-else>Loading...</div>
 </template>
