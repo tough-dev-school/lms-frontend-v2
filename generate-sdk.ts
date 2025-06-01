6db8e34be731c759e662969151425145a5b9f90d
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-06-01 21:10:36 +0200

    Fix API

--- generate-sdk.ts
+++ generate-sdk.ts
@@ -1,9 +1,10 @@
-import { rmSync, writeFileSync } from 'node:fs';
+import { rmSync, writeFileSync, readFileSync } from 'node:fs';
 import { resolve } from 'node:path';
 import { generateApi } from 'swagger-typescript-api';
 
 const OPENAPI_URL = 'http://localhost:8000/api/v2/docs/schema/?format=json';
 const SWAGGER_FILE = resolve(process.cwd(), './swagger.json');
+const GENERATED_API_FILE = resolve(process.cwd(), './src/api/generated-api.ts');
 
 const openapiData = await fetch(OPENAPI_URL).then((res) => res.json());
 
@@ -19,5 +20,13 @@ await generateApi({
   unwrapResponseData: true,
   fileName: 'generated-api',
 });
+
+const content = readFileSync(GENERATED_API_FILE, 'utf-8');
+
+const updatedContent = content.replace(
+  /descendants?\?: Answer\[\]/g,
+  'descendants: AnswerTree[]',
+);
+writeFileSync(GENERATED_API_FILE, updatedContent);
 
 rmSync(SWAGGER_FILE);
