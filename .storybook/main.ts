c9eaa5495546520330c7d88d9f84ab38c8b71357
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-06-06 16:39:26 +0200

    Disable telemetry

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,19 @@
 import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-docs",
   ],
-  "framework": {
-    "name": "@storybook/vue3-vite",
-    "options": {}
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
   }
 };
 export default config;
