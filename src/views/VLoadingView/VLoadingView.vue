6d70d0bd72f20d4f5b542524b1ae9d5e2daf14a8
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-06-06 15:11:15 +0200

    Only show loading if it takes too long

--- src/views/VLoadingView/VLoadingView.vue
+++ src/views/VLoadingView/VLoadingView.vue
@@ -3,8 +3,23 @@
   import VPublicLayout from '@/layouts/VPublicLayout/VPublicLayout.vue';
   import VLoggedLayout from '@/layouts/VLoggedLayout/VLoggedLayout.vue';
   import useAuth from '@/stores/auth';
+  import { ref, onMounted, onUnmounted } from 'vue';
 
   const { token } = useAuth();
+
+  const isTooLong = ref(true);
+
+  let timeout: ReturnType<typeof setTimeout>;
+
+  onMounted(() => {
+    timeout = setTimeout(() => {
+      isTooLong.value = false;
+    }, 300);
+  });
+
+  onUnmounted(() => {
+    clearTimeout(timeout);
+  });
 </script>
 
 <template>
