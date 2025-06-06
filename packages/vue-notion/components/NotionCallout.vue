3e95ecd20e939182cda4f13ffa833b90cff2038b
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-06-06 18:58:46 +0200

    Add vue notion

---
+++ packages/vue-notion/components/NotionCallout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="['notion-callout', blockColorClass(), blockColorClass('_co')]">
+    <div>
+      <NotionPageIcon v-bind="pass" />
+    </div>
+    <div class="notion-callout-text">
+      <div
+        v-if="block.value.content && block.value.properties?.title"
+        class="notion-callout-title">
+        <NotionTextRenderer :text="title" v-bind="pass" />
+      </div>
+
+      <template v-if="block.value.content">
+        <NotionRenderer
+          v-for="(contentId, contentIndex) in block.value.content"
+          v-bind="pass"
+          :key="contentId"
+          :level="pass.level + 1"
+          :content-id="contentId"
+          :content-index="contentIndex" />
+      </template>
+      <NotionTextRenderer v-else :text="title" v-bind="pass" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import { Blockable } from '../lib/blockable';
+  import NotionPageIcon from './NotionPageIcon.vue';
+  import NotionRenderer from './NotionRenderer.vue';
+  import NotionTextRenderer from './NotionTextRenderer.vue';
+  import { defineAsyncComponent } from 'vue';
+
+  export default {
+    components: {
+      NotionPageIcon,
+      NotionTextRenderer,
+      NotionRenderer: defineAsyncComponent(
+        () => import('./NotionRenderer.vue'),
+      ),
+    },
+    extends: Blockable,
+  };
+</script>
