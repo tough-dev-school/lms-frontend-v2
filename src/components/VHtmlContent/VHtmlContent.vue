4734f70385ddb937b919d14c7b44c1704b076875
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-06-09 11:45:51 +0200

    Break long links (#380)

--- src/components/VHtmlContent/VHtmlContent.vue
+++ src/components/VHtmlContent/VHtmlContent.vue
@@ -9,7 +9,7 @@
 <template>
   <!-- eslint-disable vue/no-v-html -->
   <!-- #TODO Check for XSS-->
-  <article class="prose prose-custom" v-html="content" />
+  <article class="prose prose-a:break-words prose-custom" v-html="content" />
 </template>
 
 <style>
