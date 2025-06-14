681ed3e1eb23665d2961fd9153a7b131fd5e6fa4
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-06-14 14:35:47 +0200

    Move redirect to composable

--- src/views/VHomeworkView/VHomeworkView.vue
+++ src/views/VHomeworkView/VHomeworkView.vue
@@ -1,12 +1,7 @@
 <script lang="ts" setup>
-  import { watch, onMounted, onBeforeMount, ref } from 'vue';
-  import { useRouter, useRoute } from 'vue-router';
-  import { useQueryClient } from '@tanstack/vue-query';
-  import {
-    fetchUser,
-    fetchHomeworkAnswer,
-    fetchHomeworkAnswers,
-  } from '@/query';
+  import { toRef } from 'vue';
+  import { useHomeworkRedirect } from '@/views/VHomeworkView/useHomeworkRedirect';
+  import VLoadingView from '@/views/VLoadingView/VLoadingView.vue';
   import VHomeworkAnswerView from './VHomeworkAnswerView.vue';
   import VHomeworkQuestionView from './VHomeworkQuestionView.vue';
 
@@ -15,49 +10,10 @@
     answerId?: string;
   }>();
 
-  const router = useRouter();
-  const route = useRoute();
-  const queryClient = useQueryClient();
-
-  const isRedirecting = ref(false);
-
-  const handleHomeworkLogic = async () => {
-    isRedirecting.value = true;
-    const user = await fetchUser(queryClient);
-
-    if (props.answerId) {
-      try {
-        await fetchHomeworkAnswer(queryClient, {
-          answerId: props.answerId,
-        });
-      } catch {
-        await router.replace({
-          ...route,
-          query: { ...route.query, answerId: undefined },
-        });
-      }
-    } else {
-      const answers = await fetchHomeworkAnswers(queryClient, {
-        questionId: props.questionId,
-        authorId: user.uuid,
-      });
-
-      if (answers && answers.length > 0) {
-        await router.replace({
-          ...route,
-          query: {
-            ...route.query,
-            answerId: answers[0]?.slug,
-          },
-        });
-      }
-    }
-    isRedirecting.value = false;
-  };
-
-  onBeforeMount(handleHomeworkLogic);
-
-  watch([() => props.questionId, () => props.answerId], handleHomeworkLogic);
+  const { isRedirecting } = useHomeworkRedirect(
+    toRef(props, 'questionId'),
+    toRef(props, 'answerId'),
+  );
 </script>
 
 <template>
---
+++ src/views/VHomeworkView/useHomeworkRedirect.ts
@@ -0,0 +1,59 @@
+import { ref, watch, onBeforeMount, type Ref } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import { useQueryClient } from '@tanstack/vue-query';
+import { fetchUser, fetchHomeworkAnswer, fetchHomeworkAnswers } from '@/query';
+
+export function useHomeworkRedirect(
+  questionId: Ref<string>,
+  answerId: Ref<string | undefined>,
+) {
+  const router = useRouter();
+  const route = useRoute();
+  const queryClient = useQueryClient();
+  const isRedirecting = ref(false);
+
+  const handleHomeworkLogic = async () => {
+    isRedirecting.value = true;
+
+    try {
+      const user = await fetchUser(queryClient);
+
+      if (answerId.value) {
+        try {
+          await fetchHomeworkAnswer(queryClient, {
+            answerId: answerId.value,
+          });
+        } catch {
+          await router.replace({
+            ...route,
+            query: { ...route.query, answerId: undefined },
+          });
+        }
+      } else {
+        const answers = await fetchHomeworkAnswers(queryClient, {
+          questionId: questionId.value,
+          authorId: user.uuid,
+        });
+
+        if (answers && answers.length > 0) {
+          await router.replace({
+            ...route,
+            query: {
+              ...route.query,
+              answerId: answers[0]?.slug,
+            },
+          });
+        }
+      }
+    } finally {
+      isRedirecting.value = false;
+    }
+  };
+
+  onBeforeMount(handleHomeworkLogic);
+  watch([questionId, answerId], handleHomeworkLogic);
+
+  return {
+    isRedirecting,
+  };
+}
