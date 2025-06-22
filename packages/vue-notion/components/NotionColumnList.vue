eb5092a80c30e8d6063092a6721776b9a1c7a306
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-06-22 11:41:30 +0200

    Dry components code

---
+++ packages/vue-notion/components/NotionColumnList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="notion-row">
+    <slot />
+  </div>
+</template>
+
+<script>
+  import { Blockable } from '../lib/blockable';
+
+  export default {
+    extends: Blockable,
+  };
+</script>
---
+++ packages/vue-notion/components/NotionSync.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="isType('transclusion_container')" class="notion-sync-block">
+    <slot />
+  </div>
+</template>
+
+<script>
+  import { Blockable } from '../lib/blockable';
+
+  export default {
+    extends: Blockable,
+  };
+</script>
