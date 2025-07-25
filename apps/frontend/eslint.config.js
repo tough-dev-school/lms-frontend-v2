53424deba0acaeab1f12a73276b8e0e0704c2278
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-07-25 04:01:06 +0200

    Disable prettier in eslint

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,16 +1,9 @@
-import { defineConfig, GLOB_SRC_EXT, GLOB_VUE } from 'eslint-config-fans';
+import { defineConfig, GLOB_VUE } from 'eslint-config-fans';
 
 export default defineConfig(
   {
     typescript: true,
     vue: true,
-    prettier: true,
-  },
-  {
-    files: [GLOB_SRC_EXT],
-    rules: {
-      'prettier/prettier': ['warn', { usePrettierrc: true }],
-    },
   },
   {
     files: [GLOB_VUE],
