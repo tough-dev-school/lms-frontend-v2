978c47a586e02318dc94ad00cbfaad031ffc193f
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-07-25 17:59:29 +0200

    Bump config

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -28,14 +28,6 @@ export default defineConfig(
       // Disabled due to legacy code patterns
       'unicorn/filename-case': 'off',
       'unicorn/prefer-string-replace-all': 'off',
-
-      // Changes for eslint-config-fans
-      // Disabled for prettier: true
-      'vue/html-indent': 'off',
-      'vue/html-self-closing': 'off',
-      'vue/html-closing-bracket-newline': 'off',
-      // Disabled because it's a too opinionated stylistic rule
-      'vue/define-macros-order': 'off',
       // Disabled because the config cannot detect the node version for some reason
       'n/no-unsupported-features/es-syntax': 'off',
     },
