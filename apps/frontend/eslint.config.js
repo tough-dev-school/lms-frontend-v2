bac08440c144ab2640a63c183ec3bac35445c55f
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-07-25 02:51:59 +0200

    Add our config

---
+++ apps/frontend/eslint.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'eslint-config-fans';
+
+export default defineConfig({
+  // Enable features based on your project
+  typescript: true,
+  vue: true,
+  prettier: true,
+});
