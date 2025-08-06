7b266bbf06073df23fde7c3d77999bed313033a2
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-08-06 09:31:18 +0200

    Fix breadcrumbs b (#431)
    * Remove random b
    
    * Remove duplicate breadcrumb

--- apps/frontend/src/utils/getNotionTitle.ts
+++ apps/frontend/src/utils/getNotionTitle.ts
@@ -8,8 +8,10 @@ const getNotionTitle = (
   // as api is unofficial and poorly typed we just assume title value is a nested array and then flatten it with the magic number to convert it into string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const getBlockTitle = (block: any): string | undefined =>
-    // eslint-disable-next-line unicorn/no-magic-array-flat-depth
-    block?.value?.properties?.title?.flat(100).join('');
+    block?.value?.properties?.title
+      ?.flat(Infinity)
+      .filter((c: string) => c !== 'b') // notion workers tends to put random b's (maybe as a sign of linebreak)
+      .join('');
 
   const blockId = idToUuid(materialId);
   const firstBlockId = Object.keys(material)[0];
