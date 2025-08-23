d31e4c378e4ef5c385a6246db5ca198723477b06
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-08-23 17:25:29 +0200

    Fix table wrapping

--- apps/frontend/src/utils/tiptap.ts
+++ apps/frontend/src/utils/tiptap.ts
@@ -1,7 +1,12 @@
 import StarterKit from '@tiptap/starter-kit';
 import { Placeholder } from '@tiptap/extensions';
 import Image from '@tiptap/extension-image';
-import { TableKit } from '@tiptap/extension-table';
+import {
+  Table,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from '@tiptap/extension-table';
 
 export const getExtensions = ({ placeholder }: { placeholder: string }) => [
   StarterKit.configure({
@@ -16,12 +21,20 @@ export const getExtensions = ({ placeholder }: { placeholder: string }) => [
     placeholder,
   }),
   Image.configure({ inline: true }),
-  TableKit.configure({
-    table: {
-      resizable: true, // this is important to get tableWrapper
-      HTMLAttributes: {
-        class: 'border-collapse table-auto w-full',
-      },
+  TableCell,
+  TableHeader,
+  TableRow,
+  // https://github.com/ueberdosis/tiptap/issues/4872#issuecomment-2717554498
+  Table.configure({
+    resizable: true,
+    HTMLAttributes: {
+      class: 'border-collapse table-auto w-full',
+    },
+  }).extend({
+    renderHTML({ node, HTMLAttributes }) {
+      const originalRender = this.parent?.({ node, HTMLAttributes });
+      const wrapper = ['div', { class: 'tableWrapper' }, originalRender];
+      return wrapper;
     },
   }),
 ];
