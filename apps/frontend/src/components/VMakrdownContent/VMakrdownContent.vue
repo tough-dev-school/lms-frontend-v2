3fe4c2f74e7741046d1fb26c239167e335f42849
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-08-24 15:55:52 +0200

    Add markdown content

---
+++ apps/frontend/src/components/VMakrdownContent/VMakrdownContent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import VHtmlContent from '../VHtmlContent/VHtmlContent.vue';
+  import { marked } from 'marked';
+  import { computed } from 'vue';
+
+  const props = defineProps<{
+    markdown: string;
+  }>();
+
+  const html = computed(() => {
+    return marked.parse(props.markdown);
+  });
+</script>
+
+<template>
+  <VHtmlContent :html="html" />
+</template>
