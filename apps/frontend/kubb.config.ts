f61c6f42ae0feb03926acdcbbfa9a22ff791d261
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-11-15 13:07:45 +0100

    Generate mocks

--- apps/frontend/kubb.config.ts
+++ apps/frontend/kubb.config.ts
@@ -4,51 +4,57 @@ import { pluginMsw } from '@kubb/plugin-msw';
 import { pluginTs } from '@kubb/plugin-ts';
 import { pluginClient } from '@kubb/plugin-client';
 import { pluginVueQuery } from '@kubb/plugin-vue-query';
+import { pluginFaker } from '@kubb/plugin-faker';
 
 const banner = '/* eslint-disable */\n// @ts-nocheck';
 
-export default defineConfig(() => {
-  return {
-    root: '.',
-    input: {
-      path: 'http://localhost:8000/api/v2/docs/schema/',
-    },
-    output: {
-      path: './src/api/generated',
-      clean: true,
-    },
-    plugins: [
-      pluginOas({
-        generators: [],
-      }),
-      pluginTs({
-        output: {
-          path: './types',
-          banner,
-        },
-      }),
-      pluginClient({
+export default defineConfig({
+  root: '.',
+  input: {
+    path: 'http://localhost:8000/api/v2/docs/schema/',
+  },
+  output: {
+    path: './src/api/generated',
+    clean: true,
+  },
+  plugins: [
+    pluginOas({
+      generators: [],
+    }),
+    pluginTs({
+      output: {
+        path: './types',
+        banner,
+      },
+    }),
+    pluginClient({
+      importPath: '../../client.ts',
+      output: {
+        path: './clients',
+        banner,
+      },
+    }),
+    pluginMsw({
+      output: {
+        path: './handlers',
+        banner,
+      },
+    }),
+    pluginFaker({
+      output: {
+        path: './mocks',
+        banner,
+      },
+      seed: 1337,
+    }),
+    pluginVueQuery({
+      client: {
         importPath: '../../client.ts',
-        output: {
-          path: './clients',
-          banner,
-        },
-      }),
-      pluginMsw({
-        output: {
-          path: './handlers',
-          banner,
-        },
-      }),
-      pluginVueQuery({
-        client: {
-          importPath: '../../client.ts',
-        },
-        output: {
-          path: './hooks',
-          banner,
-        },
-      }),
-    ],
-  };
-});
+      },
+      output: {
+        path: './hooks',
+        banner,
+      },
+    }),
+  ],
+}) as ReturnType<typeof defineConfig>;
