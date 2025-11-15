da9b5d21c157b33596e6294c01e5db62df248112
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-11-15 13:46:07 +0100

    Add custom client

---
+++ apps/frontend/src/api/client.ts
@@ -0,0 +1,52 @@
+import onResponseRejected from './onResponseRejected';
+import onRequestFulfilled from './onRequestFulfilled';
+import axios from 'axios';
+
+export interface RequestConfig<TData = unknown> {
+  url?: string;
+  method: 'GET' | 'PUT' | 'PATCH' | 'POST' | 'DELETE';
+  params?: object;
+  data?: TData | FormData;
+  responseType?:
+    | 'arraybuffer'
+    | 'blob'
+    | 'document'
+    | 'json'
+    | 'text'
+    | 'stream';
+  signal?: AbortSignal;
+  headers?: HeadersInit;
+}
+
+export interface ResponseConfig<TData = unknown> {
+  data: TData;
+  status: number;
+  statusText: string;
+}
+
+export const client = async <TData, TError = unknown, TVariables = unknown>(
+  config: RequestConfig<TVariables>,
+): Promise<ResponseConfig<TData>> => {
+  const httpClient = axios.create();
+
+  httpClient.interceptors.request.use((value) => onRequestFulfilled(value));
+
+  httpClient.interceptors.response.use(
+    (value) => value,
+    (value) => onResponseRejected(value),
+  );
+
+  const response = await httpClient({
+    method: config.method,
+    url: config.url,
+    data: config.data,
+    signal: config.signal,
+    headers: config.headers,
+  });
+
+  return {
+    data: response.data,
+    status: response.status,
+    statusText: response.statusText,
+  };
+};
