17661375a1c1dd674d0021eda4a70d216d3d65a3
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-11-16 20:01:50 +0100

    Remove mobile actions variant

--- apps/frontend/src/components/VAnswerActions/VAnswerActions.stories.ts
+++ apps/frontend/src/components/VAnswerActions/VAnswerActions.stories.ts
@@ -1,6 +1,5 @@
 import type { Meta, StoryFn } from '@storybook/vue3-vite';
 import VAnswerActions from '@/components/VAnswerActions/VAnswerActions.vue';
-import dayjs from 'dayjs';
 
 export default {
   title: 'AnswerActions/VAnswerActions',
@@ -19,7 +18,8 @@ export const Default = {
   render: Template,
 
   args: {
-    created: dayjs().toISOString(),
+    allowDelete: true,
+    allowEdit: true,
     deleteTime: 10,
     editTime: 30,
   },
@@ -29,7 +29,8 @@ export const DeletePassed = {
   render: Template,
 
   args: {
-    created: dayjs().toISOString(),
+    allowDelete: false,
+    allowEdit: true,
     deleteTime: 0,
     editTime: 30,
   },
@@ -39,8 +40,9 @@ export const AllPassed = {
   render: Template,
 
   args: {
-    created: dayjs().toISOString(),
-    deleteTime: 0,
-    editTime: 0,
+    allowDelete: false,
+    allowEdit: false,
+    deleteTime: 10,
+    editTime: 30,
   },
 };
--- apps/frontend/src/components/VAnswerActions/VAnswerActions.vue
+++ apps/frontend/src/components/VAnswerActions/VAnswerActions.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-  import VAnswerActionsDesktop from '@/components/VAnswerActionsDesktop/VAnswerActionsDesktop.vue';
-  import VAnswerActionsMobile from '@/components/VAnswerActionsMobile/VAnswerActionsMobile.vue';
+  import { IconTrash, IconPencilPlus } from '@tabler/icons-vue';
+  import VButton from '../VButton/VButton.vue';
 
   const emit = defineEmits<{
     delete: [];
@@ -9,16 +9,24 @@
 </script>
 
 <template>
-  <div class="flex gap-8">
-    <VAnswerActionsDesktop
-      class="hidden tablet:flex"
-      data-testid="desktop"
-      @edit="emit('edit')"
-      @delete="emit('delete')" />
-    <VAnswerActionsMobile
-      class="tablet:hidden"
-      data-testid="mobile"
-      @edit="emit('edit')"
-      @delete="emit('delete')" />
+  <div class="flex items-center gap-8">
+    <VButton
+      appearance="secondary"
+      size="inline"
+      class="flex aspect-square h-32 items-center justify-center phone:aspect-auto phone:px-16"
+      data-testid="delete"
+      @click="emit('delete')"
+    >
+      <IconTrash />
+    </VButton>
+    <VButton
+      appearance="secondary"
+      size="inline"
+      class="flex aspect-square h-32 items-center justify-center phone:aspect-auto phone:px-16"
+      data-testid="edit"
+      @click="emit('edit')"
+    >
+      <IconPencilPlus />
+    </VButton>
   </div>
 </template>
