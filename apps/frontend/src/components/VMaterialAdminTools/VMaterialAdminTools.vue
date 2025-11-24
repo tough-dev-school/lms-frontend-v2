ed44058805142976c2a9d93511d2d248b9e10b17
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-11-24 18:52:30 +0100

    Add formats

--- apps/frontend/src/components/VMaterialAdminTools/VMaterialAdminTools.vue
+++ apps/frontend/src/components/VMaterialAdminTools/VMaterialAdminTools.vue
@@ -9,7 +9,7 @@
   } from '@/query';
   import { useQueryClient } from '@tanstack/vue-query';
   import { useIntervalFn } from '@vueuse/core';
-  import { isBefore } from '@/utils/date';
+  import { DATE_TIME_SECONDS_FORMAT, formatDate, isBefore } from '@/utils/date';
   import { computed } from 'vue';
 
   const props = defineProps<{
@@ -27,13 +27,36 @@
   );
 
   const inProgress = computed(() => {
+    if (materialStatus.value?.fetch_complete === null) return true;
     return isBefore(
       materialStatus.value?.fetch_started,
       materialStatus.value?.fetch_complete,
     );
   });
 
+  const fetchStarted = computed(() => {
+    return materialStatus.value?.fetch_started
+      ? formatDate(
+          materialStatus.value.fetch_started,
+          DATE_TIME_SECONDS_FORMAT,
+          true,
+        )
+      : '-';
+  });
+
+  const fetchComplete = computed(() => {
+    return materialStatus.value?.fetch_complete
+      ? formatDate(
+          materialStatus.value.fetch_complete,
+          DATE_TIME_SECONDS_FORMAT,
+          true,
+        )
+      : '-';
+  });
+
   useIntervalFn(() => {
+    if (!inProgress.value) return;
+
     queryClient.invalidateQueries({
       queryKey: materialsKeys.status(props.materialId),
     });
@@ -45,8 +68,8 @@
     v-if="user?.is_staff"
     class="flex flex-col gap-8 rounded-16 bg-white p-8 dark:bg-dark-black phone:p-24"
   >
-    <p>Последнее обновление запущено {{ materialStatus?.fetch_started }}</p>
-    <p>Материал актуален на {{ materialStatus?.fetch_complete }}</p>
+    <p>Последнее обновление: {{ fetchStarted }}</p>
+    <p>Материал актуален: {{ fetchComplete }}</p>
     <template #footer>
       <VButton @click="() => updateMaterial(props.materialId)">
         <template v-if="inProgress">Обновление в процессе</template>
