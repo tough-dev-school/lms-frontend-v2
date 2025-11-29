5029b4c927316743d925caa910e9dd1db4fca506
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-11-29 16:14:53 +0100

    Output errors

--- apps/frontend/src/components/VError/VError.vue
+++ apps/frontend/src/components/VError/VError.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-  defineProps<{
-    error: unknown;
+  import { computed } from 'vue';
+  import { pick } from 'lodash-es';
+
+  const props = defineProps<{
+    error: { response: { data: Record<string, string[]> } } | null;
+    whitelist?: string[];
   }>();
+
+  const errors = computed(() => {
+    if (!props.error?.response?.data) return {};
+
+    const data = props.error.response.data;
+
+    return props.whitelist
+      ? (pick(data, props.whitelist) as Record<string, unknown>)
+      : data;
+  });
 </script>
 
 <template>
-  <pre>{{ JSON.stringify(error, null, 2) }}</pre>
+  <ul
+    v-if="Object.keys(errors).length > 0"
+    class="origin-left"
+  >
+    <li
+      v-for="(error, key) in errors"
+      :key="key"
+      class="text-red"
+    >
+      {{ key }}: {{ Array.isArray(error) ? error.join(', ') : error }}
+    </li>
+  </ul>
 </template>
