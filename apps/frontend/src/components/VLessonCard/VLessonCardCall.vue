b12b7f7a27712a83a047a08a23bc0e2cfd186b2c
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-12-05 18:24:49 +0100

    Clean up VLessonCard logic

---
+++ apps/frontend/src/components/VLessonCard/VLessonCardCall.vue
@@ -0,0 +1,72 @@
+<script lang="ts" setup>
+  import VHeading from '@/components/VHeading/VHeading.vue';
+  import VTag from '@/components/VTag/VTag.vue';
+  import VButton from '@/components/VButton/VButton.vue';
+  import type {
+    Call,
+    RecommendedVideoProviderEnum,
+    VideoProvider,
+  } from '@/api/generated/generated-api';
+  import { DATE_TIME_FORMAT, formatDate } from '@/utils/date';
+
+  defineProps<{
+    call: Call;
+  }>();
+
+  const getRecommendedVideo = (
+    videos: VideoProvider[],
+    recommendedVideoProvider: RecommendedVideoProviderEnum | null,
+  ) => {
+    const fallback = videos[0];
+    if (!recommendedVideoProvider) return fallback;
+    return (
+      videos.find((v) => v.provider === recommendedVideoProvider) || fallback
+    );
+  };
+</script>
+
+<template>
+  <div class="flex flex-col gap-16">
+    <div class="flex flex-wrap gap-8 empty:hidden">
+      <VTag v-if="call.datetime">
+        {{ formatDate(call.datetime, DATE_TIME_FORMAT) }}
+      </VTag>
+    </div>
+    <VHeading
+      tag="h3"
+      class="mb-8"
+    >
+      {{ call.name }}
+    </VHeading>
+    <p v-if="call.description">{{ call.description }}</p>
+  </div>
+
+  <div class="flex-grow"></div>
+
+  <template v-if="call.video.length">
+    <a
+      v-if="
+        getRecommendedVideo(call.video, call.recommended_video_provider).src
+      "
+      :href="
+        getRecommendedVideo(call.video, call.recommended_video_provider).src
+      "
+      target="_blank"
+    >
+      <VButton class="VLessonCard__Button"> Смотреть запись </VButton>
+    </a>
+  </template>
+  <template v-else-if="call.url">
+    <a
+      :href="call.url"
+      target="_blank"
+    >
+      <VButton
+        class="VLessonCard__Button"
+        appearance="secondary"
+      >
+        Подключиться
+      </VButton>
+    </a>
+  </template>
+</template>
---
+++ apps/frontend/src/components/VLessonCard/VLessonCardHomework.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+  import VHeading from '@/components/VHeading/VHeading.vue';
+  import VTag from '@/components/VTag/VTag.vue';
+  import VButton from '@/components/VButton/VButton.vue';
+  import type { Homework, Question } from '@/api/generated/generated-api';
+  import { DATE_TIME_FORMAT, formatDate } from '@/utils/date';
+
+  defineProps<{
+    homework: Homework;
+    question: Question;
+  }>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-16">
+    <div class="flex flex-wrap gap-8 empty:hidden">
+      <VTag v-if="question.deadline">
+        Дедлайн
+        {{ formatDate(question.deadline, DATE_TIME_FORMAT) }}
+      </VTag>
+    </div>
+    <VHeading
+      tag="h3"
+      class="mb-8"
+    >
+      {{ question.name }}
+    </VHeading>
+  </div>
+
+  <div>
+    <table class="w-full">
+      <tr>
+        <td class="font-bold">Статус</td>
+        <td class="whitespace-nowrap text-right">
+          {{ homework.is_sent ? 'Отправлена' : 'Не отправлена' }}
+        </td>
+      </tr>
+      <tr v-if="homework.crosschecks">
+        <td class="font-bold">Проверенные домашки коллег</td>
+        <td class="text-right">
+          {{ homework.crosschecks.checked }} из
+          {{ homework.crosschecks.total }}
+        </td>
+      </tr>
+    </table>
+  </div>
+
+  <div class="flex-grow"></div>
+
+  <RouterLink
+    :to="{
+      name: 'homework',
+      params: { questionId: question.slug },
+    }"
+  >
+    <VButton class="VLessonCard__Button"> Открыть </VButton>
+  </RouterLink>
+</template>
